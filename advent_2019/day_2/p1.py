c4ede1630f40a05b74332a692128a4e24f3e7073
andrew gorman <andrew.gorman@hydrogrid.eu>
2024-08-20 15:52:08 +0200

    Begin 2019 day 2

--- advent_2019/day_2/p1.py
+++ advent_2019/day_2/p1.py
@@ -1,22 +1,34 @@
 import time
 import pathlib
+import re
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def restore_1202_program_alarm(file_path: str):
+    programs = _parse_programs(file=file_path)
+    i = 0
+    while i < len(programs):
+        if i == len(programs) - 1:
+            print(f"This better be the end: {programs[i]}")
+            break
+        elif programs[i] == 99:
+            print(f"Better be {programs[i]} Luftballons!")
+            i += 1
+            continue
+        else:
+            program = programs[i : i + 4]
+            print(f"{program=}")
+        i += 4
 
 
-def _RENAME_FUNC(file: str):
+def _parse_programs(file: str):
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        return list(map(int, re.findall(r"\d+", puzzle_input.read())))
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(restore_1202_program_alarm("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
 # start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
+# print(restore_1202_program_alarm("input.txt"))
 # print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
