f787e807cb913f48dc47a7abd059fef0ba39cc06
andrew gorman <andrew.gorman@hydrogrid.eu>
2024-08-29 08:31:32 +0200

    complete day_3 part 1 with naive solution untidy begin part 2

--- advent_2019/day_3/p1.py
+++ advent_2019/day_3/p1.py
@@ -2,21 +2,75 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def find_manhattan_closest_intersection(file_path: str) -> int:
+    wires = _parse_wires(file=file_path)
+    all_visited = list()
+    for wire in wires:
+        visited = set()
+        x = 0
+        y = 0
+        current_node = [x, y]
+        for instruction in wire.split(","):
+            direction = instruction[0]
+            spaces = int(instruction[1:])
+            if direction == "i":
+                if spaces >= 0:
+                    p = 0
+                    while p < spaces:
+                        new_node = current_node[0] + 1, current_node[1]
+                        visited.add(new_node)
+                        current_node = new_node
+                        p += 1
+                else:
+                    p = 0
+                    while p > spaces:
+                        new_node = current_node[0] - 1, current_node[1]
+                        visited.add(new_node)
+                        current_node = new_node
+                        p -= 1
+            else:
+                assert direction == "j"
+                if spaces >= 0:
+                    p = 0
+                    while p < spaces:
+                        new_node = current_node[0], current_node[1] + 1
+                        visited.add(new_node)
+                        current_node = new_node
+                        p += 1
+                else:
+                    p = 0
+                    while p > spaces:
+                        new_node = current_node[0], current_node[1] - 1
+                        visited.add(new_node)
+                        current_node = new_node
+                        p -= 1
+        all_visited.append(visited)
+    intersections = set.intersection(*all_visited)
+    return min([_manhattan(node) for node in intersections])
 
 
-def _RENAME_FUNC(file: str):
+def _manhattan(node: tuple[int, int]) -> int:
+    return abs(node[0]) + abs(node[1])
+
+
+def _parse_wires(file: str):
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        lines = (
+            puzzle_input.read()
+            .replace("R", "i")
+            .replace("U", "j")
+            .replace("L", "i-")
+            .replace("D", "j-")
+            .strip()
+            .splitlines()
+        )
+        return lines
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(find_manhattan_closest_intersection("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(find_manhattan_closest_intersection("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
--- advent_2019/day_3/p2.py
+++ advent_2019/day_3/p2.py
@@ -1,22 +1,80 @@
 import time
 import pathlib
+from collections import defaultdict
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def find_manhattan_closest_intersection(file_path: str) -> int:
+    wires = _parse_wires(file=file_path)
+    all_visited = list()
+    for wire in wires:
+        visited: dict[int, set[tuple[int, int]]] = defaultdict(set)
+        steps = 0
+        x = 0
+        y = 0
+        current_node = [x, y]
+        for instruction in wire.split(","):
+            direction = instruction[0]
+            spaces = int(instruction[1:])
+            if direction == "i":
+                if spaces >= 0:
+                    p = 0
+                    while p < spaces:
+                        steps += 1
+                        new_node = current_node[0] + 1, current_node[1]
+                        visited[steps].add(new_node)
+                        current_node = new_node
+                        p += 1
+                else:
+                    p = 0
+                    while p > spaces:
+                        steps += 1
+                        new_node = current_node[0] - 1, current_node[1]
+                        visited[steps].add(new_node)
+                        current_node = new_node
+                        p -= 1
+            else:
+                assert direction == "j"
+                if spaces >= 0:
+                    p = 0
+                    while p < spaces:
+                        steps += 1
+                        new_node = current_node[0], current_node[1] + 1
+                        visited[steps].add(new_node)
+                        current_node = new_node
+                        p += 1
+                else:
+                    p = 0
+                    while p > spaces:
+                        steps += 1
+                        new_node = current_node[0], current_node[1] - 1
+                        visited[steps].add(new_node)
+                        current_node = new_node
+                        p -= 1
+        all_visited.append(visited.values())
+    print(f"{all_visited=}")
+    # intersections = set.intersection(*all_visited.values())
+    # print(f"{len(intersections)=}")
+    return 0
 
 
-def _RENAME_FUNC(file: str):
+def _parse_wires(file: str):
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        lines = (
+            puzzle_input.read()
+            .replace("R", "i")
+            .replace("U", "j")
+            .replace("L", "i-")
+            .replace("D", "j-")
+            .strip()
+            .splitlines()
+        )
+        return lines
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(find_manhattan_closest_intersection("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
 # start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
+# print(find_manhattan_closest_intersection("input.txt"))
 # print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
