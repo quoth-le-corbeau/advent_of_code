bf9318e255d5a7ee64eec05c4f4f8eeb1c666322
quoth-the-macbook <amgorman@tutanota.com>
2024-10-17 21:13:36 +0200

    Complete day4 semi-naive

--- advent_2019/day_4/p1.py
+++ advent_2019/day_4/p1.py
@@ -2,13 +2,25 @@ import time
 import pathlib
 
 
-def count_possible_passwords(file_path: str):
+def count_possible_passwords(file_path: str) -> int:
     with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
         password_range_map = tuple(map(int, puzzle_input.read().strip().split("-")))
-        # define mathematical condition for counting number of possible repeated digits
-        # define mathematical condition for counting number of possible numbers with increasing digits
+        count = 0
+        for n in range(password_range_map[0], password_range_map[1]):
+            if str(n) == "".join(sorted(str(n))) and _contains_adjacent_pair(
+                num_string=str(n)
+            ):
+                count += 1
+        return count
 
 
+def _contains_adjacent_pair(num_string: str) -> bool:
+    i = 0
+    while i < len(num_string) - 1:
+        if num_string[i + 1] == num_string[i]:
+            return True
+        i += 1
+    return False
 
 
 start = time.perf_counter()
--- advent_2019/day_4/p2.py
+++ advent_2019/day_4/p2.py
@@ -2,21 +2,35 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def count_possible_passwords_2(file_path: str) -> int:
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        password_range_map = tuple(map(int, puzzle_input.read().strip().split("-")))
+        count = 0
+        for n in range(password_range_map[0], password_range_map[1]):
+            if str(n) == "".join(sorted(str(n))) and _contains_adjacent_exact_pair(
+                num_string=str(n)
+            ):
+                count += 1
+        return count
 
 
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+def _contains_adjacent_exact_pair(num_string: str) -> bool:
+    i = 0
+    end_index = len(num_string) - 1
+    count = 0
+    while i < end_index:
+        if num_string[i + 1] != num_string[i] and count == 1:
+            return True
+        elif num_string[i + 1] == num_string[i]:
+            count += 1
+        else:
+            count = 0
+        i += 1
+    if count == 1:
+        return True
+    return False
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
-print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
-
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+print(count_possible_passwords_2("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
