10888c78f1d4c54df218c9b75270d7721ee37a44
andrew gorman <andrew.gorman@hydrogrid.eu>
2024-10-17 11:24:31 +0200

    Begin day4

--- advent_2019/day_4/p1.py
+++ advent_2019/day_4/p1.py
@@ -2,21 +2,15 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def count_possible_passwords(file_path: str):
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        password_range_map = tuple(map(int, puzzle_input.read().strip().split("-")))
+        # define mathematical condition for counting number of possible repeated digits
+        # define mathematical condition for counting number of possible numbers with increasing digits
 
 
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
-print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
-
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+print(count_possible_passwords("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
