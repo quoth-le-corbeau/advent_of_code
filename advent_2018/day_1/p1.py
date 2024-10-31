0898ccc58bd6e05ccf7752eac92fbd79b8554a7d
quoth-the-macbook <amgorman@tutanota.com>
2024-10-31 15:17:46 +0000

    Complete da1

--- advent_2018/day_1/p1.py
+++ advent_2018/day_1/p1.py
@@ -3,20 +3,22 @@ import pathlib
 
 
 def calculate_resulting_freq(file_path: str) -> int:
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        return sum([int(entry) for entry in puzzle_input.read().splitlines()])
 
 
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+def calculate_resulting_freq_v2(file_path: str) -> int:
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        result = 0
+        for entry in puzzle_input.read().splitlines():
+            result += int(entry)
+    return result
 
 
 start = time.perf_counter()
 print(calculate_resulting_freq("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(calculate_resulting_freq("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
--- advent_2018/day_1/p2.py
+++ advent_2018/day_1/p2.py
@@ -2,21 +2,28 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def find_first_freq_reached_twice(file_path: str) -> int:
+    entries = _parse_entries(file=file_path)
+    res = 0
+    seen = set()
+    i = 0
+    while True:
+        res += int(entries[i % len(entries)])
+        if res in seen:
+            return res
+        seen.add(res)
+        i += 1
 
 
-def _RENAME_FUNC(file: str):
+def _parse_entries(file: str) -> list[str]:
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        return puzzle_input.read().splitlines()
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(find_first_freq_reached_twice("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(find_first_freq_reached_twice("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
