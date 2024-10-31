de3ad6704a82226b895ff206cc15684c8cb5c5ce
quoth-the-macbook <amgorman@tutanota.com>
2024-10-31 21:23:45 +0000

    Complete day1p1

--- advent_2018/day_2/p1.py
+++ advent_2018/day_2/p1.py
@@ -2,21 +2,38 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def calculate_checksum(file_path: str) -> int:
+    letter_lines = _parse_input(file=file_path)
+    two_count = 0
+    three_count = 0
+    for line in letter_lines:
+        two = False
+        three = False
+        for char in set(line):
+            if line.count(char) == 2:
+                two = True
+                continue
+            if line.count(char) == 3:
+                three = True
+                continue
+        if two:
+            two_count += 1
+        if three:
+            three_count += 1
+    print(two_count)
+    print(three_count)
+    return three_count * two_count
 
 
-def _RENAME_FUNC(file: str):
+def _parse_input(file: str) -> list[str]:
     with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        return puzzle_input.read().splitlines()
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(calculate_checksum("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(calculate_checksum("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
