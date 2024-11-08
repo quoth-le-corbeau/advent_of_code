60f79f052969bca6fae202a459b93742590231b6
andrew gorman <andrew.gorman@hydrogrid.ai>
2024-11-08 16:05:41 +0100

    use re to do day_2p2

--- advent_2018/day_2/p2.py
+++ advent_2018/day_2/p2.py
@@ -1,15 +1,29 @@
 import time
 import pathlib
+from typing import Optional
+import re
 
 
 def get_common_box_id_letters(file_path: str) -> str:
     with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
         box_ids = puzzle_input.read().splitlines()
         for i, box_id in enumerate(box_ids):
-            letters = box_id.split()
-            print(letters)
             for b_id in box_ids[i + 1 :]:
-                pass
+                if single_difference(box_id, b_id) is not None:
+                    print(single_difference(box_id, b_id))
+                    return box_id.replace(single_difference(box_id, b_id), "")
+        return "EPIC FAIL"
+
+
+def single_difference(s1: str, s2: str) -> Optional[str]:
+    if len(s1) != len(s2):
+        raise ValueError("s1 and s2 do not have the same length")
+    for i in range(len(s1)):
+        pattern = re.escape(s1[:i]) + "." + re.escape(s1[i + 1 :])
+        match = re.fullmatch(pattern, s2)
+        if match is not None:
+            return s1[i]
+    return None
 
 
 start = time.perf_counter()
