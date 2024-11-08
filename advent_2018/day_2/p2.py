41e7c19693fff66b383666ef6ef5e9706aa1e594
andrew gorman <andrew.gorman@hydrogrid.ai>
2024-11-08 15:04:03 +0100

    WIP - discard set-based algo

--- advent_2018/day_2/p2.py
+++ advent_2018/day_2/p2.py
@@ -6,14 +6,10 @@ def get_common_box_id_letters(file_path: str) -> str:
     with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
         box_ids = puzzle_input.read().splitlines()
         for i, box_id in enumerate(box_ids):
-            for b_id in box_ids[i + 1:]:
-                common_letters = set(box_id).intersection(b_id)
-                if abs(len(common_letters) - len(box_id)) == 1:
-                    print(f"{len(common_letters) - len(box_id)=}")
-                if len(common_letters) == len(box_id) - 1:
-                    print(common_letters)
-                    char = set(box_id).difference(common_letters).pop()
-                    return box_id.replace(char, "")
+            letters = box_id.split()
+            print(letters)
+            for b_id in box_ids[i + 1 :]:
+                pass
 
 
 start = time.perf_counter()
