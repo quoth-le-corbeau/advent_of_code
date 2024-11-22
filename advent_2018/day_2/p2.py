46d1c13db957bf301afd028d852b7ca2b73449cf
quoth-the-macbook <amgorman@tutanota.com>
2024-11-22 21:09:51 +0100

    my dict solution

--- advent_2018/day_2/p2.py
+++ advent_2018/day_2/p2.py
@@ -6,8 +6,16 @@ def get_common_box_id_letters(file_path: str) -> str:
     with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
         box_ids = puzzle_input.read().splitlines()
         for i, box_id in enumerate(box_ids):
-            for b_id in box_ids[i + 1 :]:
-                pass
+            box_dict = {k: box_id[k] for k in range(len(box_id))}
+            for remaining_id in box_ids[i + 1 :]:
+                remaining_dict = {k: remaining_id[k] for k in range(len(remaining_id))}
+                diff = set(box_dict.items()) - set(remaining_dict.items())
+                if len(diff) == 1:
+                    print(type(diff))
+                    index, _ = diff.pop()
+                    print(f"{index}")
+                    print(f"{box_id}")
+                    return box_id[:index] + box_id[index:]
 
 
 
