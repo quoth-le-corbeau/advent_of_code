4b45eb23b1c95de4ee458b37a07a9bb7ef7a8a6e
quoth-the-macbook <amgorman@tutanota.com>
2024-11-30 21:53:06 +0100

    2017 day_1

--- advent_2017/day_1/p1.py
+++ advent_2017/day_1/p1.py
@@ -2,21 +2,29 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
-
-
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+def sum_same_adjacent_digits(file_path: str) -> int:
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        lines = puzzle_input.read().strip()
+        first = int(lines[0])
+        total = 0
+        for index, char in enumerate(lines):
+            if index == len(lines) - 1:
+                if int(char) == first:
+                    total += int(char)
+                    return total
+                else:
+                    return total
+            else:
+                if char == lines[index + 1]:
+                    total += int(char)
+                else:
+                    continue
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(sum_same_adjacent_digits("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(sum_same_adjacent_digits("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
--- advent_2017/day_1/p2.py
+++ advent_2017/day_1/p2.py
@@ -2,21 +2,31 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
-
-
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+def sum_digits_a_step_away(file_path: str) -> int:
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        lines = puzzle_input.read().strip()
+        total = 0
+        step = len(lines) // 2
+        for index, char in enumerate(lines):
+            if index == len(lines) - 1:
+                if int(char) == int(lines[step]):
+                    return total + int(char)
+                else:
+                    return total
+            elif index >= len(lines) - step:
+                if int(char) == int(lines[(step + index) % len(lines)]):
+                    total += int(char)
+            else:
+                if char == lines[index + step]:
+                    total += int(char)
+                else:
+                    continue
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(sum_digits_a_step_away("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(sum_digits_a_step_away("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
