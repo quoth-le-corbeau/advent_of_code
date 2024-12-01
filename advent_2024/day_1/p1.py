181e130918076f49109e949654a7cd580118df9a
quoth-the-macbook <amgorman@tutanota.com>
2024-12-01 08:38:15 +0100

    star 2

--- advent_2024/day_1/p1.py
+++ advent_2024/day_1/p1.py
@@ -1,22 +1,13 @@
 import time
-import pathlib
 import numpy as np
+from advent_2024.day_1 import utils
 
 
 def sum_location_differences(file_path: str) -> int:
-    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
-        lines = puzzle_input.read().splitlines()
-        locations_1 = []
-        locations_2 = []
-        for line in lines:
-            location_1, location_2 = tuple(map(int, line.replace("   ",",").split(",")))
-            locations_1.append(location_1)
-            locations_2.append(location_2)
-        l1 = np.array(sorted(locations_1))
-        l2 = np.array(sorted(locations_2))
-        print(abs(l2 - l1))
-        return sum(abs(l2 - l1))
-
+    locations_1, locations_2 = utils.parse_into_lists(file_path)
+    l1 = np.array(sorted(locations_1))
+    l2 = np.array(sorted(locations_2))
+    return sum(abs(l2 - l1))
 
 
 start = time.perf_counter()
--- advent_2024/day_1/p2.py
+++ advent_2024/day_1/p2.py
@@ -1,22 +1,20 @@
 import time
-import pathlib
+
+from advent_2024.day_1 import utils
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
-
-
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+def calculate_similarity_score(file_path: str) -> int:
+    locations_1, locations_2 = utils.parse_into_lists(file_path)
+    similarity_score = 0
+    for location in locations_1:
+        similarity_score += location * locations_2.count(location)
+    return similarity_score
 
 
 start = time.perf_counter()
-print(RENAME_FUNC("eg.txt"))
+print(calculate_similarity_score("eg.txt"))
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC("input.txt"))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(calculate_similarity_score("input.txt"))
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
