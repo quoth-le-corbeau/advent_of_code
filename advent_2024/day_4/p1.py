d913372a25b8feec0333da32f3db4ed1690418c7
quoth-the-macbook <amgorman@tutanota.com>
2024-12-04 07:00:45 +0100

    star 7

--- advent_2024/day_4/p1.py
+++ advent_2024/day_4/p1.py
@@ -1,21 +1,42 @@
 import time
 import pathlib
+import pandas
+
+_XMAS = ["XMAS", "SAMX"]
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def find_xmas(file_path: str) -> int:
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        df = pandas.DataFrame([list(line) for line in puzzle_input.read().splitlines()])
+        count = 0
+        for _, row in df.iterrows():
+            row_string = "".join(row)
+            print(f"{row_string=}")
+            for xmas in _XMAS:
+                count += row_string.count(xmas)
+        for _, col in df.transpose().iterrows():
+            col_string = "".join(col)
+            for xmas in _XMAS:
+                count += col_string.count(xmas)
+        for diagonal in _get_diagonals(df):
+            for xmas in _XMAS:
+                count += diagonal.count(xmas)
+    return count
 
 
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+def _get_diagonals(df: pandas.DataFrame) -> list[str]:
+    diagonals = []
+    for offset in range(-df.shape[0] + 1, df.shape[1]):
+        diagonals.append(''.join(df.values.diagonal(offset)))
+    flipped_df = df.iloc[:, ::-1]
+    for offset in range(-flipped_df.shape[0] + 1, flipped_df.shape[1]):
+        diagonals.append(''.join(flipped_df.values.diagonal(offset)))
+    return diagonals
 
 
 start = time.perf_counter()
 print(
-    RENAME_FUNC(
+    find_xmas(
         str(
             (
                 pathlib.Path(__file__).resolve().parents[2]
@@ -29,7 +50,7 @@ print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
 start = time.perf_counter()
 print(
-    RENAME_FUNC(
+    find_xmas(
         str(
             (
                 pathlib.Path(__file__).resolve().parents[2]
