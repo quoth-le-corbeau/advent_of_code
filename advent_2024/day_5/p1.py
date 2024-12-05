3e78294d2bf068b6c07aa32e907e8a72b7c880a6
andrew gorman <andrew.gorman@hydrogrid.ai>
2024-12-05 11:16:49 +0100

    WIP

--- advent_2024/day_5/p1.py
+++ advent_2024/day_5/p1.py
@@ -5,21 +5,44 @@ import re
 
 def sum_middle_pages(file_path: str) -> int:
     rules, updates = _parse_input(file=file_path)
-    print(f"{rules=}")
-    print(f"{updates=}")
-    order = _get_order(rules)
+    order_map = _get_order_map(rules)
+    print(f"{order_map=}")
     middles = 0
     for update in updates:
-        if _is_ordered(update, order):
-            middles += update[len(update)//2 + 1]
+        if len(update) == 0:
+            continue
+        if _is_ordered(list_to_check=update, order_map=order_map):
+            print(f"{update} is an ordered update")
+            middles += update[len(update) // 2]
     return middles
 
-def _is_ordered(update: list[int], order: list[int]) -> bool:
-    is_ordered = False
-    return is_ordered
-def _get_order(rules: list[tuple[int, int]]) -> list[int]:
-    result = []
-    return result
+
+def _is_ordered(list_to_check: list[int], order_map: dict[int, int]) -> bool:
+    idx_list = [order_map[n] for n in list_to_check if n in order_map]
+    return sorted(idx_list) == idx_list
+
+
+def _get_order_map(rules: list[tuple[int, int]]) -> dict[int, int]:
+    ordered = []
+    for rule in rules:
+        x, y = rule
+        if not (x in ordered or y in ordered):
+            ordered += [x, y]
+        if x in ordered and not y in ordered:
+            index = ordered.index(x)
+            ordered.insert(index + 1, y)
+        if x not in ordered and y in ordered:
+            index = ordered.index(y)
+            ordered.insert(index, x)
+        if x in ordered and y in ordered:
+            x_index = ordered.index(x)
+            y_index = ordered.index(y)
+            if x_index > y_index:
+                ordered.remove(x)
+                ordered.insert(y_index, x)
+            else:
+                continue
+    return {n: ordered.index(n) for n in ordered}
 
 
 def _parse_input(file: str):
@@ -35,6 +58,7 @@ def _parse_input(file: str):
         ]
         return rules, updates
 
+
 start = time.perf_counter()
 print(
     sum_middle_pages(
@@ -49,6 +73,16 @@ print(
 )
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(sum_middle_pages(str((pathlib.Path(__file__).resolve().parents[2] / "my_inputs/2024/day_5" / "input.txt"))))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(
+    sum_middle_pages(
+        str(
+            (
+                pathlib.Path(__file__).resolve().parents[2]
+                / "my_inputs/2024/day_5"
+                / "input.txt"
+            )
+        )
+    )
+)
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
