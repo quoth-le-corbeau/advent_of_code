da3e2db0d0f34782f2da0666724a629492421c75
andrew gorman <andrew.gorman@hydrogrid.ai>
2024-12-05 11:47:46 +0100

    star 9

--- advent_2024/day_5/p1.py
+++ advent_2024/day_5/p1.py
@@ -5,44 +5,22 @@ import re
 
 def sum_middle_pages(file_path: str) -> int:
     rules, updates = _parse_input(file=file_path)
-    order_map = _get_order_map(rules)
-    print(f"{order_map=}")
     middles = 0
     for update in updates:
         if len(update) == 0:
             continue
-        if _is_ordered(list_to_check=update, order_map=order_map):
-            print(f"{update} is an ordered update")
+        ordered = True
+        for i, n in enumerate(update):
+            if not ordered:
+                break
+            relevant_rules = [rule for rule in rules if n == rule[1]]
+            for other in update[i + 1 :]:
+                if any([other == rule[0] for rule in relevant_rules]):
+                    ordered = False
+                    break
+        if ordered:
             middles += update[len(update) // 2]
     return middles
-
-
-def _is_ordered(list_to_check: list[int], order_map: dict[int, int]) -> bool:
-    idx_list = [order_map[n] for n in list_to_check if n in order_map]
-    return sorted(idx_list) == idx_list
-
-
-def _get_order_map(rules: list[tuple[int, int]]) -> dict[int, int]:
-    ordered = []
-    for rule in rules:
-        x, y = rule
-        if not (x in ordered or y in ordered):
-            ordered += [x, y]
-        if x in ordered and not y in ordered:
-            index = ordered.index(x)
-            ordered.insert(index + 1, y)
-        if x not in ordered and y in ordered:
-            index = ordered.index(y)
-            ordered.insert(index, x)
-        if x in ordered and y in ordered:
-            x_index = ordered.index(x)
-            y_index = ordered.index(y)
-            if x_index > y_index:
-                ordered.remove(x)
-                ordered.insert(y_index, x)
-            else:
-                continue
-    return {n: ordered.index(n) for n in ordered}
 
 
 def _parse_input(file: str):
