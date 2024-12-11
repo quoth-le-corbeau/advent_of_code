22fcab06bee016b35ed3aff95dd3ed919ff87a82
andrew gorman <andrew.gorman@hydrogrid.ai>
2024-12-11 09:56:33 +0100

    star 21

--- advent_2024/day_11/p1.py
+++ advent_2024/day_11/p1.py
@@ -1,21 +1,71 @@
 import time
 import pathlib
 
-
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+"""
+Plutonian Pebbles Part I
 
 
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+read in the input string
+create a function that takes in a string and outputs a new string
+initialize a counter at 0
+run the function 25 times
+functionality:
+create a dictionary of each element with its index as key:
+    e.g. {
+            1: "1250"
+            2: "17"
+            3: "0"
+            4: "11"
+            5: "1"
+         }
+        result should be
+            1: "12 50"
+            2: "1 7"
+            3: "1"
+            4: "1 1"
+            5: "2024"
+from this the next string is created:
+"".join(val) for val in d.values()
+
+finally return the length of the last output string.split(" ")
+
+"""
+
+
+def _apply_rules(row: str) -> str:
+    stone_by_idx = {idx: stone for idx, stone in enumerate(row.split(" "))}
+    new_stone_by_idx = {idx: "" for idx in stone_by_idx}
+    for position, stone in stone_by_idx.items():
+        if stone == "0":
+            new_stone_by_idx[position] += "1"
+        elif len(stone) % 2 == 0:
+            new_stone_by_idx[position] += (
+                stone[: len(stone) // 2] + " " + str(int(stone[len(stone) // 2 :]))
+            )
+        else:
+            new_stone_by_idx[position] += str(int(stone) * 2024)
+    new_string = ""
+    for value in new_stone_by_idx.values():
+        for v in value.split(" "):
+            new_string += v
+            new_string += " "
+    return new_string.strip()
+
+
+def count_the_stones_after_25(file_path: str):
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        row = puzzle_input.read().strip()
+
+    counter = 0
+    while counter < 75:
+        row = _apply_rules(row)
+        counter += 1
+    return len(row.split(" "))
 
 
 start = time.perf_counter()
 print(
-    RENAME_FUNC(
+    count_the_stones_after_25(
         str(
             (
                 pathlib.Path(__file__).resolve().parents[2]
@@ -27,6 +77,16 @@ print(
 )
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC(str((pathlib.Path(__file__).resolve().parents[2] / "my_inputs/2024/day_11" / "input.txt"))))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(
+    count_the_stones_after_25(
+        str(
+            (
+                pathlib.Path(__file__).resolve().parents[2]
+                / "my_inputs/2024/day_11"
+                / "input.txt"
+            )
+        )
+    )
+)
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
