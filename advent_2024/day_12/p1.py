7a5b7be9a4f758d7cf6be2f7205abbf2bfeb3a12
andrew gorman <andrew.gorman@hydrogrid.ai>
2024-12-12 09:10:36 +0100

    WIP

--- advent_2024/day_12/p1.py
+++ advent_2024/day_12/p1.py
@@ -1,21 +1,28 @@
 import time
 import pathlib
 
-
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+"""
+Garden Groups Part I
 
 
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+
+"""
+
+
+def get_garden_group_price(file_path: str) -> int:
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        lines = puzzle_input.read().strip()
+        plant_types = set(lines)
+        garden_groups = {type: 0 for type in plant_types if type != "\n"}
+
+        print(f"{garden_groups=}")
+        grid = [line for line in lines.split("\n")]
+        print(f"{grid=}")
 
 
 start = time.perf_counter()
 print(
-    RENAME_FUNC(
+    get_garden_group_price(
         str(
             (
                 pathlib.Path(__file__).resolve().parents[2]
@@ -28,5 +35,5 @@ print(
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
 # start = time.perf_counter()
-# print(RENAME_FUNC(str((pathlib.Path(__file__).resolve().parents[2] / "my_inputs/2024/day_12" / "input.txt"))))
+# print(get_garden_group_price(str((pathlib.Path(__file__).resolve().parents[2] / "my_inputs/2024/day_12" / "input.txt"))))
 # print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
