3799ee3060035a85e329b7c7c9afcc43c9428732
andrew gorman <andrew.gorman@hydrogrid.ai>
2024-12-21 14:26:08 +0100

    star 31

--- advent_2024/day_18/p1.py
+++ advent_2024/day_18/p1.py
@@ -1,108 +1,97 @@
-import heapq
-import time
 import pathlib
+import time
+from collections import deque
+
+"""
+Ram Run Part I
+
+create a grid n x n with specified n
+read in the fallen bytes (switch to row, col)
+add # to grid for cut off number of fallen bytes
+calculate best path from top left to bottom right with bfs
+return the length of the best path
+
+"""
 
 
-def _pprint(grid: list[list[str]], message: str = "") -> None:
-    print(message)
-    for line in grid:
-        print("".join(line))
+class MemorySpace:
+    def __init__(self, size: int):
+        self.grid = [["."] * size for _ in range(size)]
+
+    def add_bytes_to_grid(self, fallen_bytes: list[str], cutoff: int):
+        for coordinates in fallen_bytes[:cutoff]:
+            r, c = int(coordinates.split(",")[1]), int(coordinates.split(",")[0])
+            self.grid[r][c] = "#"
+
+    def show_path(self, path: list[tuple[int, int]]) -> None:
+        for node in path:
+            self.grid[node[0]][node[1]] = ">"
+        self.pprint(message="Showing best path:")
+
+    def pprint(self, message: str = ""):
+        print(message)
+        for row in self.grid:
+            print("".join(row))
 
 
-def _prepare_grid(
-    fallen_byte_coordinates: list[str],
-    number_of_fallen_bytes: int,
-    rows: int,
-    cols: int,
-) -> list[list[str]]:
-    grid = [["."] * cols for _ in range(rows)]
-    for coordinates in fallen_byte_coordinates[:number_of_fallen_bytes]:
-        r, c = int(coordinates.split(",")[1]), int(coordinates.split(",")[0])
-        grid[r][c] = "#"
-    return grid
-
-
-def _a_star(
-    grid: list[list[str]],
-    start: tuple[int, int],
-    end: tuple[int, int],
-    fallen_bytes: int,
-) -> list[tuple[int, int]]:
+def _bfs(grid: list[list[str]]) -> list[tuple[int, int]]:
     rows = len(grid)
     cols = len(grid[0])
+    start_ = (0, 0)
+    end_ = (rows - 1, cols - 1)
 
-    def heuristic(node: tuple[int, int], end: tuple[int, int]) -> int:
-        return abs(node[0] - end[0]) + abs(node[1] - end[1])
+    queue = deque([[start_]])
+    visited = {start_}
 
-    pq = []
-    heapq.heappush(pq, (0, start, [start]))
-    visited = set()
+    while queue:
+        path = queue.popleft()
+        current = path[-1]
 
-    while pq:
-        cost, current, path = heapq.heappop(pq)
-        if current == end:
-            for p in path:
-                grid[p[0]][p[1]] = "O"
-            _pprint(grid, message=f"Shortest path after {fallen_bytes} fallen bytes: ")
+        if current == end_:
             return path
 
-        for direction in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        for direction in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             next_ = current[0] + direction[0], current[1] + direction[1]
             if 0 <= next_[0] < rows and 0 <= next_[1] < cols:
                 if grid[next_[0]][next_[1]] != "#" and next_ not in visited:
-                    heapq.heappush(
-                        pq, (cost + 1 + heuristic(next_, end), next_, path + [next_])
-                    )
+                    queue.append(path + [next_])
                     visited.add(next_)
+    raise Exception("No path found")
 
-    raise ValueError("No path found!")
 
-
-def find_best_escape_path(
-    file_path: str, rows: int = 71, cols: int = 71, fallen_bytes: int = 1024
-) -> int:
-    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
-        grid = _prepare_grid(
-            fallen_byte_coordinates=puzzle_input.read().splitlines(),
-            number_of_fallen_bytes=fallen_bytes,
-            rows=rows,
-            cols=cols,
-        )
-        _pprint(grid=grid, message=f"Grid after {fallen_bytes} fallen bytes: ")
-        best_escape_path = _a_star(
-            grid=grid, start=(0, 0), end=(rows - 1, cols - 1), fallen_bytes=fallen_bytes
-        )
-        print(f"{best_escape_path=}")
-        return len(best_escape_path) - 1  # exclude start
+def part_one(input: str, size: int, cutoff: int) -> int:
+    with open(input, mode="r") as f:
+        lines = f.read().strip().splitlines()
+        memory_space = MemorySpace(size=size)
+        # memory_space.pprint(message="original grid")
+        memory_space.add_bytes_to_grid(fallen_bytes=lines, cutoff=cutoff)
+        # memory_space.pprint(message=f"grid after {cutoff} fallen bytes")
+        path = _bfs(memory_space.grid)
+        memory_space.show_path(path=path)
+        return len(path) - 1
 
 
 start = time.perf_counter()
-print(
-    find_best_escape_path(
-        str(
-            (
-                pathlib.Path(__file__).resolve().parents[2]
-                / "my_inputs/2024/day_18"
-                / "eg.txt"
-            )
-        ),
-        rows=7,
-        cols=7,
-        fallen_bytes=12,
-    )
+result_eg = part_one(
+    input=str(
+        pathlib.Path(__file__).resolve().parents[2] / "my_inputs/2024/day_18" / "eg.txt"
+    ),
+    size=7,
+    cutoff=12,
 )
-print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+
+print(f"Part 1 example: {result_eg}")
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
 start = time.perf_counter()
-print(
-    find_best_escape_path(
-        str(
-            (
-                pathlib.Path(__file__).resolve().parents[2]
-                / "my_inputs/2024/day_18"
-                / "input.txt"
-            )
-        )
-    )
+result = part_one(
+    input=str(
+        pathlib.Path(__file__).resolve().parents[2]
+        / "my_inputs/2024/day_18"
+        / "input.txt"
+    ),
+    size=71,
+    cutoff=1024,
 )
+print(f"Part 2 example: {result}")
 print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
