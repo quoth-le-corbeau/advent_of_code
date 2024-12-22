fabd403e7f5c9323801d4ba7bc9f7bddda76e3c6
andrew gorman <andrew.gorman@hydrogrid.ai>
2024-12-22 10:59:55 +0100

    star 33

--- advent_2024/day_17/p1.py
+++ advent_2024/day_17/p1.py
@@ -10,74 +10,76 @@ class Computer:
         self.register_C = register_C
         self.output = ""
         self.operand_map = {
-            0: {"combo": 0, "literal": 0},
-            1: {"combo": 1, "literal": 1},
-            2: {"combo": 2, "literal": 2},
-            3: {"combo": 3, "literal": 3},
-            4: {"combo": self.register_A, "literal": 4},
-            5: {"combo": self.register_B, "literal": 5},
-            6: {"combo": self.register_C, "literal": 6},
-            7: {"combo": ValueError, "literal": 7},
+            0: {"combo": lambda: 0, "literal": 0},
+            1: {"combo": lambda: 1, "literal": 1},
+            2: {"combo": lambda: 2, "literal": 2},
+            3: {"combo": lambda: 3, "literal": 3},
+            4: {"combo": lambda: self.register_A, "literal": 4},
+            5: {"combo": lambda: self.register_B, "literal": 5},
+            6: {"combo": lambda: self.register_C, "literal": 6},
+            7: {
+                "combo": lambda: (_ for _ in ()).throw(
+                    ValueError("Invalid combo for 7")
+                ),
+                "literal": 7,
+            },
         }
         self.instruction_map = {
-            0: self.adv,
-            1: self.bxl,
-            2: self.bst,
-            3: self.jnz,
-            4: self.bxc,
-            5: self.out,
-            6: self.bdv,
-            7: self.cdv,
+            0: self._adv,
+            1: self._bxl,
+            2: self._bst,
+            3: self._jnz,
+            4: self._bxc,
+            5: self._out,
+            6: self._bdv,
+            7: self._cdv,
         }
 
-    def parse_program(self, program: list[int]) -> list[tuple[int, int]]:
-        sub_programs = []
-        for i in range(len(program) - 1):
-            if program[i] == 3 and self.register_A == 0:
-                try:
-                    sub_programs.append((program[i + 1], program[i + 2]))
-                except IndexError:
-                    raise IndexError("wtfk dude?")
+    def run(self, program: list[int]) -> str:
+        i = 0
+        while i < len(program) - 1:
+            if program[i] == 3 and self.register_A != 0:
+                i = self._jnz(operand=program[i + 1])
+                continue
             else:
-                sub_programs.append((program[i], program[i + 1]))
-        return sub_programs
+                op_code, operand = program[i], program[i + 1]
+                self.instruction_map[op_code](operand)
+                i += 2
+        if len(self.output) > 0:
+            return self.output[:-1]
+        else:
+            assert self.output == ""
+            return self.output
 
-    def run(self, program: list[int]) -> None:
-        sub_programs = self.parse_program(program)
-        for sub_program in sub_programs:
-            op_code, operand = sub_program
-            self.instruction_map[op_code](operand)
-
-    def adv(self, operand: int):
+    def _adv(self, operand: int):
         numerator = self.register_A
-        denominator = 2 ** self.operand_map[operand]["combo"]
+        denominator = 2 ** self.operand_map[operand]["combo"]()
         self.register_A = numerator // denominator
 
-    def bxl(self, operand: int):
+    def _bxl(self, operand: int):
         self.register_B = self.register_B ^ self.operand_map[operand]["literal"]
 
-    def bst(self, operand: int):
-        self.register_B = self.operand_map[operand]["combo"] % 8
+    def _bst(self, operand: int):
+        self.register_B = self.operand_map[operand]["combo"]() % 8
 
-    def bxc(self, operand: int):
+    def _bxc(self, operand: int):
         print(f"Ignoring {operand} for legacy reasons")
         self.register_B = self.register_B ^ self.register_C
 
-    def jnz(self, operand: int):
-        print(f"operand 3 with regist")
-        pass
+    def _jnz(self, operand: int):
+        return self.operand_map[operand]["literal"]
 
-    def out(self, operand: int):
-        self.output += str(self.operand_map[operand]["combo"] % 8) + ","
+    def _out(self, operand: int):
+        self.output += str(self.operand_map[operand]["combo"]() % 8) + ","
 
-    def bdv(self, operand):
+    def _bdv(self, operand):
         numerator = self.register_A
-        denominator = 2 ** self.operand_map[operand]["combo"]
+        denominator = 2 ** self.operand_map[operand]["combo"]()
         self.register_B = numerator // denominator
 
-    def cdv(self, operand: int):
+    def _cdv(self, operand: int):
         numerator = self.register_A
-        denominator = 2 ** self.operand_map[operand]["combo"]
+        denominator = 2 ** self.operand_map[operand]["combo"]()
         self.register_C = numerator // denominator
 
 
@@ -89,8 +91,8 @@ def chronospatial_output(file_path: str):
         computer = Computer(
             register_A=registers[0], register_B=registers[1], register_C=registers[2]
         )
-        computer.run(program)
-        return computer.output
+        output = computer.run(program)
+        return output
 
 
 start = time.perf_counter()
@@ -107,6 +109,16 @@ print(
 )
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(chronospatial_output(str((pathlib.Path(__file__).resolve().parents[2] / "my_inputs/2024/day_17" / "input.txt"))))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(
+    chronospatial_output(
+        str(
+            (
+                pathlib.Path(__file__).resolve().parents[2]
+                / "my_inputs/2024/day_17"
+                / "input.txt"
+            )
+        )
+    )
+)
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
