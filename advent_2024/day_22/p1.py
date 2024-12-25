28468094732314e22f3e542624607011bd5fa4ad
quoth-the-macbook <amgorman@tutanota.com>
2024-12-25 11:08:11 +0100

    star 36

--- advent_2024/day_22/p1.py
+++ advent_2024/day_22/p1.py
@@ -2,20 +2,28 @@ import time
 import pathlib
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def _get_next_secret_number(secret_number: int) -> int:
+    step_one = ((secret_number * 64) ^ secret_number) % 16777216
+    step_two = ((step_one // 32) ^ step_one) % 16777216
+    return ((step_two * 2048) ^ step_two) % 16777216
 
 
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+def find_2000th_secret_number(file_path: str) -> int:
+    with open(pathlib.Path(__file__).parent / file_path, "r") as puzzle_input:
+        secret_starts = list(map(int, puzzle_input.read().splitlines()))
+        total = 0
+        for ss in secret_starts:
+            secret = ss
+            for _ in range(2000):
+                secret = _get_next_secret_number(secret_number=secret)
+            print(f"secret_start: {ss}, 2000th: {secret}")
+            total += secret
+        return total
 
 
 start = time.perf_counter()
 print(
-    RENAME_FUNC(
+    find_2000th_secret_number(
         str(
             (
                 pathlib.Path(__file__).resolve().parents[2]
@@ -27,6 +35,16 @@ print(
 )
 print(f"TEST -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
 
-# start = time.perf_counter()
-# print(RENAME_FUNC(str((pathlib.Path(__file__).resolve().parents[2] / "my_inputs/2024/day_22" / "input.txt"))))
-# print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
+start = time.perf_counter()
+print(
+    find_2000th_secret_number(
+        str(
+            (
+                pathlib.Path(__file__).resolve().parents[2]
+                / "my_inputs/2024/day_22"
+                / "input.txt"
+            )
+        )
+    )
+)
+print(f"REAL -> Elapsed {time.perf_counter() - start:2.4f} seconds.")
