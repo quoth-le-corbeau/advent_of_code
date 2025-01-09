f8e8020dd0c17ebf45ee4a82fce75fbb6f2a6837
quoth-the-macbook <amgorman@tutanota.com>
2025-01-09 01:07:39 +0100

    star 41

--- advent_2024/day_23/p1.py
+++ advent_2024/day_23/p1.py
@@ -1,32 +1,60 @@
-import time
-import pathlib
+from collections import defaultdict
+from pathlib import Path
+from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: str):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+class Network:
+    def __init__(self, file: Path):
+        with open(
+            file=Path(__file__).resolve().parents[2] / file, mode="r"
+        ) as puzzle_input:
+            connection_pairs = [
+                line.split("-") for line in puzzle_input.read().strip().splitlines()
+            ]
+            self.connections = defaultdict(set)
+            for pair in connection_pairs:
+                c1, c2 = pair[0], pair[1]
+                self.connections[c1].add(c2)
+                self.connections[c2].add(c1)
+
+    def find_three_connected(self):
+        triples = set()
+        for node, connected in self.connections.items():
+            for c1 in connected:
+                for c2 in connected:
+                    if (
+                        c1 != c2
+                        and node in self.connections[c1]
+                        and c2 in self.connections[c1]
+                        and node in self.connections[c2]
+                        and c1 in self.connections[c2]
+                        and any(x.startswith("t") for x in [node, c1, c2])
+                    ):
+                        triple = sorted([node, c1, c2])
+                        triples.add(tuple(triple))
+        # for triple in triples:
+        #    print(triple)
+        return len(triples)
+
+    def find_most_connected(self):
+        pass
 
 
-def _RENAME_FUNC(file: str):
-    with open(pathlib.Path(__file__).parent / file, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+@timer
+def part_one(filename: str, year: int = 2024, day: int = 23):
+    input_path = INPUT_PATH.format(file=filename, year=year, day=day)
+    network = Network(file=input_path)
+    print(network.find_three_connected())
 
 
-timer_start = time.perf_counter()
-print(
-    RENAME_FUNC(
-        str(
-            (
-                pathlib.Path(__file__).resolve().parents[2]
-                / "my_inputs/2024/day_23"
-                / "eg.txt"
-            )
-        )
-    )
-)
-print(f"TEST -> Elapsed {time.perf_counter() - timer_start:2.4f} seconds.")
+# part_one("input")
 
-# timer_start = time.perf_counter()
-# print(RENAME_FUNC(str((pathlib.Path(__file__).resolve().parents[2] / "my_inputs/2024/day_23" / "input.txt"))))
-# print(f"REAL -> Elapsed {time.perf_counter() - timer_start:2.4f} seconds.")
+
+@timer
+def part_two(filename: str, year: int = 2024, day: int = 23):
+    input_path = INPUT_PATH.format(file=filename, year=year, day=day)
+    network = Network(file=input_path)
+    print(network.find_most_connected())
+
+
+part_two("eg")
