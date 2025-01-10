dfa87244a3bc15e01bdb61f9614fcbbc32dc2244
= <=>
2025-01-10 23:32:20 +0100

    star 42

--- advent_2024/day_23/solutions.py
+++ advent_2024/day_23/solutions.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pathlib import Path
 from reusables import timer, INPUT_PATH
 
@@ -8,66 +8,64 @@ class Network:
         with open(
             file=Path(__file__).resolve().parents[2] / file, mode="r"
         ) as puzzle_input:
-            connection_pairs = [
+            self.edges = [
                 line.split("-") for line in puzzle_input.read().strip().splitlines()
             ]
-            self.connections = defaultdict(set)
-            for pair in connection_pairs:
+
+            self.connections_by_node = defaultdict(set)
+            for pair in self.edges:
                 c1, c2 = pair[0], pair[1]
-                self.connections[c1].add(c2)
-                self.connections[c2].add(c1)
+                self.connections_by_node[c1].add(c2)
+                self.connections_by_node[c2].add(c1)
 
     def find_three_connected(self):
         triples = set()
-        for node, connected in self.connections.items():
+        for node, connected in self.connections_by_node.items():
             for c1 in connected:
                 for c2 in connected:
                     if (
                         c1 != c2
-                        and node in self.connections[c1]
-                        and c2 in self.connections[c1]
-                        and node in self.connections[c2]
-                        and c1 in self.connections[c2]
+                        and node in self.connections_by_node[c1]
+                        and c2 in self.connections_by_node[c1]
+                        and node in self.connections_by_node[c2]
+                        and c1 in self.connections_by_node[c2]
                         and any(x.startswith("t") for x in [node, c1, c2])
                     ):
                         triple = sorted([node, c1, c2])
                         triples.add(tuple(triple))
         return len(triples)
 
-    def crack_password_fail_for_real_input(self) -> str:
-        for key, connected in self.connections.items():
-            for conn in connected:
-                self.connections[conn].remove(key)
+    def crack_password(self) -> str:
+        groups = []
+        for edge in self.edges:
+            group = self.get_group(edge=edge)
+            if (len(group), sorted(group)) not in groups:
+                groups.append((len(group), sorted(group)))
+        size, largest_group = sorted(groups, reverse=True)[0]
+        print(f"The largest group is {largest_group} of size: {size}")
+        result_group = sorted(largest_group)
+        return ",".join(result_group)
 
-        largest_group_size = 0
-        largest_group = {}
-        for key, values in self.connections.items():
-            values_list = list(values)
-            group = {key}
-            for i, val in enumerate(values_list):
-                for other in values_list[:i] + values_list[i + 1 :]:
-                    if other in self.connections[val] or val in self.connections[other]:
-                        group.add(other)
-                        group.add(val)
+    def get_group(self, edge: list[str]) -> list[str]:
+        group = set()
+        group.add(edge[0])
+        group.add(edge[1])
+        queue = deque(edge)
 
-                    if len(group) > largest_group_size:
-                        largest_group_size = len(group)
-                        largest_group = group
-
-        print(largest_group_size, largest_group)
-        alphabetical_group = sorted(list(largest_group))
-        return ",".join(alphabetical_group)
-
-    def generate_edges(self):
-        edges = []
-        # for each node in self.connections
-        for node in self.connections:
-
-            # for each neighbour node of a single node
-            for neighbour in self.connections[node]:
-                # if edge exists then append
-                edges.append((node, neighbour))
-        return edges
+        while len(queue) > 0:
+            node = queue.popleft()
+            connections = self.connections_by_node[node]
+            for connection in connections:
+                if (
+                    all(
+                        connection in self.connections_by_node[member]
+                        for member in group
+                    )
+                    and connection not in group
+                ):
+                    group.add(connection)
+                    queue.append(connection)
+        return list(group)
 
 
 @timer
@@ -77,6 +75,7 @@ def part_one(filename: str, year: int = 2024, day: int = 23):
     print(f"part one: {network.find_three_connected()}")
 
 
+# part_one("eg")
 part_one("input")
 
 
@@ -84,11 +83,8 @@ part_one("input")
 def part_two(filename: str, year: int = 2024, day: int = 23):
     input_path = INPUT_PATH.format(file=filename, year=year, day=day)
     network = Network(file=input_path)
-    edges = network.generate_edges()
-    print(f"{edges=}")
-    # password = network.crack_password_fail_for_real_input()
-    # print(password)
+    print(network.crack_password())
 
 
-part_two("eg")
+# part_two("eg")
 part_two("input")
