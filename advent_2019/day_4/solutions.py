1317ca0efda7f5e1ba393847faf338ad154305cd
quoth-the-macbook <amgorman@tutanota.com>
2025-05-17 23:45:13 +0200

    wip

--- advent_2019/day_4/solutions.py
+++ advent_2019/day_4/solutions.py
@@ -3,10 +3,9 @@ from pathlib import Path
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_input(file_path: Path):
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        return tuple(map(int, puzzle_input.read().strip().split("-")))
 
 
 @timer
@@ -14,11 +13,12 @@ def part_one(file: str, day: int = 4, year: int = 2019):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    print(f"<-----------{input_file_path} -------------->")
-    print(f"part 1: {RENAME_FUNC(file_path=input_file_path)}")
+    interval = _parse_input(file_path=input_file_path)
+    print(interval)
+    for n in range(*interval):
+        print(n)
 
 
-part_one(file="eg")
 part_one(file="input")
 
 
@@ -27,8 +27,7 @@ def part_two(file: str, day: int = 4, year: int = 2019):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    print(f"part 2: {RENAME_FUNC(file_path=input_file_path)}")
+    print(f"part 2: {_parse_input(file_path=input_file_path)}")
 
 
-part_two(file="eg")
 part_two(file="input")
