fa5190052d810140ac2f6fb7e3d6b8fbdc8980bc
andrew-x1-ubu <amgorman@tutanota.com>
2025-05-18 16:22:44 +0200

    part 2 solutions

--- advent_2019/day_4/solutions.py
+++ advent_2019/day_4/solutions.py
@@ -2,10 +2,23 @@ from pathlib import Path
 
 from reusables import timer, INPUT_PATH
 
+_DIGITS = "0,1,2,3,4,5,6,7,8,9".split(",")
+
 
 def _parse_input(file_path: Path) -> tuple[str, str]:
     with open(file_path, "r") as puzzle_input:
         return tuple(puzzle_input.read().strip().split("-"))
+
+
+def _has_exactly_two_adjacent_and_ascending_digits(number_str: str) -> int:
+    if "".join(sorted(number_str)) != number_str:
+        return 0
+    for digit in _DIGITS:
+        if number_str.count(digit) == 2:
+            i = number_str.index(digit)
+            if i != 5 and number_str[i + 1] == digit:
+                return 1
+    return 0
 
 
 @timer
@@ -32,7 +45,11 @@ def part_two(file: str, day: int = 4, year: int = 2019):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    print(f"part 2: {_parse_input(file_path=input_file_path)}")
+    start_str, end_str = _parse_input(file_path=input_file_path)
+    count = 0
+    for n in range(int(start_str), int(end_str)):
+        count += _has_exactly_two_adjacent_and_ascending_digits(str(n))
+    return count
 
 
 part_two(file="input")
