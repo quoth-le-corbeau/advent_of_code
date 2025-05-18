57970d973cdf52e48024a98df6f7648089cc8bac
andrew-x1-ubu <amgorman@tutanota.com>
2025-05-18 15:46:01 +0200

    day 4 p1 optimized

--- advent_2019/day_4/solutions.py
+++ advent_2019/day_4/solutions.py
@@ -3,20 +3,25 @@ from pathlib import Path
 from reusables import timer, INPUT_PATH
 
 
-def _parse_input(file_path: Path):
+def _parse_input(file_path: Path) -> tuple[str, str]:
     with open(file_path, "r") as puzzle_input:
-        return tuple(map(int, puzzle_input.read().strip().split("-")))
+        return tuple(puzzle_input.read().strip().split("-"))
 
 
 @timer
-def part_one(file: str, day: int = 4, year: int = 2019):
+def part_one(file: str, day: int = 4, year: int = 2019) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    interval = _parse_input(file_path=input_file_path)
-    print(interval)
-    for n in range(*interval):
-        print(n)
+    start_str, end_str = _parse_input(file_path=input_file_path)
+    count = 0
+    for n in range(int(start_str), int(end_str)):
+        n_str = str(n)
+        if len(set(n_str)) != len(n_str) and all(
+            [int(n_str[i + 1]) - int(n_str[i]) >= 0 for i in range(len(n_str[:-1]))]
+        ):
+            count += 1
+    return count
 
 
 part_one(file="input")
