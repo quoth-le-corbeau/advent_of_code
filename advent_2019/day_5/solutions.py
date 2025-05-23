f0aa4e70dea19d2978e6a541fce7afa97a148eef
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-05-23 13:05:18 +0200

    prepare for part 2

--- advent_2019/day_5/solutions.py
+++ advent_2019/day_5/solutions.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 from reusables import timer, INPUT_PATH
 
-_INPUT = 1
+_AIR_CONDITIONER_UNIT_ID = 1
+_THERMAL_RADIATOR_UNIT_ID = 5
 
 
 def _parse_file(file_path: Path) -> list[int]:
@@ -10,14 +11,13 @@ def _parse_file(file_path: Path) -> list[int]:
         return list(map(int, puzzle_input.read().strip().split(",")))
 
 
-def _run(program: list[int]) -> int:
+def _run_1(program: list[int], input_code: int) -> int:
     output = -1
-    i = 0
-    while i < len(program) - 2:
-        position_zero = str(program[i])
+    pointer = 0
+    while pointer < len(program) - 4:
+        position_zero = str(program[pointer])
         while len(position_zero) < 5:
             position_zero = "0" + position_zero
-        assert len(position_zero) == 5
         opcode = int(position_zero[-2:])
         mode_1 = position_zero[-3]
         mode_2 = position_zero[-4]
@@ -25,31 +25,106 @@ def _run(program: list[int]) -> int:
             print("Final diagnostic output: ")
             break
         elif opcode == 1:
-            param_1 = program[program[i + 1]] if mode_1 == "0" else program[i + 1]
-            param_2 = program[program[i + 2]] if mode_2 == "0" else program[i + 2]
-            program[program[i + 3]] = param_1 + param_2
-            i += 4
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            program[program[pointer + 3]] = param_1 + param_2
+            pointer += 4
         elif opcode == 2:
-            param_1 = program[program[i + 1]] if mode_1 == "0" else program[i + 1]
-            param_2 = program[program[i + 2]] if mode_2 == "0" else program[i + 2]
-            program[program[i + 3]] = param_1 * param_2
-            i += 4
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            program[program[pointer + 3]] = param_1 * param_2
+            pointer += 4
         elif opcode == 3:
-            program[program[i + 1]] = _INPUT
-            i += 2
+            program[program[pointer + 1]] = input_code
+            pointer += 2
         elif opcode == 4:
-            param_1 = program[program[i + 1]] if mode_1 == "0" else program[i + 1]
+            param_1 = (
+                program[program[pointer + 1]] if mode_1 == "0" else program[pointer + 1]
+            )
             output = param_1
             if output == 0:
                 print(
-                    f"Test at position {i} success! {round(((i + 1) / len(program)) * 100, ndigits=2)}%"
+                    f"Test at position {pointer} success! {round(((pointer + 1) / len(program)) * 100, ndigits=2)}%"
                 )
-            i += 2
+            pointer += 2
         else:
             raise ValueError(f"Invalid opcode: {opcode}")
     if output == -1:
         raise ValueError("No diagnostic output")
     return output
+
+
+def _run_2(program: list[int], input_code: int) -> int:
+    output = -1
+    pointer = 0
+    while pointer < len(program) - 4:
+        position_zero = str(program[pointer])
+        while len(position_zero) < 5:
+            position_zero = "0" + position_zero
+        opcode = int(position_zero[-2:])
+        mode_1 = position_zero[-3]
+        mode_2 = position_zero[-4]
+        if opcode == 99:
+            print("Final diagnostic output: ")
+            break
+        elif opcode == 1:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            program[program[pointer + 3]] = param_1 + param_2
+            pointer += 4
+        elif opcode == 2:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            program[program[pointer + 3]] = param_1 * param_2
+            pointer += 4
+        elif opcode == 3:
+            program[program[pointer + 1]] = input_code
+            pointer += 2
+        elif opcode == 4:
+            param_1 = (
+                program[program[pointer + 1]] if mode_1 == "0" else program[pointer + 1]
+            )
+            output = param_1
+            if output == 0:
+                print(
+                    f"Test at position {pointer} success! {round(((pointer + 1) / len(program)) * 100, ndigits=2)}%"
+                )
+            pointer += 2
+        elif opcode == 5:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            if param_1 != 0:
+                pointer = param_2
+            else:
+                pointer += 3
+        elif opcode == 6:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            if param_1 == 0:
+                pointer = param_2
+            else:
+                pointer += 3
+        elif opcode == 7:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            if param_1 < param_2:
+                program[program[pointer + 3]] = 1
+            else:
+                program[program[pointer + 3]] = 0
+            pointer += 4
+        elif opcode == 8:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            if param_1 == param_2:
+                program[program[pointer + 3]] = 1
+            else:
+                program[program[pointer + 3]] = 0
+            pointer += 4
+        else:
+            raise ValueError(f"Invalid opcode: {opcode}")
+    if output == -1:
+        raise ValueError("No diagnostic output")
+    return output
+
+
+def _get_param_values(
+    pointer: int, mode_1: str, mode_2: str, program: list[int]
+) -> tuple[int, int]:
+    param_1 = program[program[pointer + 1]] if mode_1 == "0" else program[pointer + 1]
+    param_2 = program[program[pointer + 2]] if mode_2 == "0" else program[pointer + 2]
+    return param_1, param_2
 
 
 @timer
@@ -58,12 +133,11 @@ def part_one(file: str, day: int = 5, year: int = 2019) -> int:
         year=year, day=day, file=file
     )
     program = _parse_file(input_file_path)
-    print(f"{len(program)=}")
-    return _run(program)
+    return _run_1(program=program, input_code=_AIR_CONDITIONER_UNIT_ID)
 
 
 # part_one(file="eg")
-part_one(file="input")
+# part_one(file="input")
 
 
 @timer
@@ -72,7 +146,8 @@ def part_two(file: str, day: int = 5, year: int = 2019):
         year=year, day=day, file=file
     )
     program = _parse_file(file_path=input_file_path)
+    return _run_2(program=program, input_code=_THERMAL_RADIATOR_UNIT_ID)
 
 
 # part_two(file="eg")
-# part_two(file="input")
+part_two(file="input")
