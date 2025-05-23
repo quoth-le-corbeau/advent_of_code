440f853e8aa651b818e832e65d8061cb5e9bb0ed
quoth-the-macbook <amgorman@tutanota.com>
2025-05-23 21:28:52 +0200

    day 6

--- advent_2019/day_6/solutions.py
+++ advent_2019/day_6/solutions.py
@@ -14,18 +14,23 @@ def _parse_orbit_map(file_path: Path) -> dict[str, str]:
 
 
 @timer
-def part_one(file: str, day: int = 6, year: int = 2019):
+def part_one(file: str, day: int = 6, year: int = 2019) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
     orbit_map = _parse_orbit_map(file_path=input_file_path)
-    print(f"{orbit_map=}")
-    print(orbit_map.keys())
-    print(orbit_map.values())
+    checksum = 0
+    for orbiter, orbited in orbit_map.items():
+        checksum += 1
+        current_key = orbited
+        while current_key in orbit_map.keys():
+            checksum += 1
+            current_key = orbit_map[current_key]
+    return checksum
 
 
 part_one(file="eg")
-# part_one(file="input")
+part_one(file="input")
 
 
 @timer
@@ -33,7 +38,34 @@ def part_two(file: str, day: int = 6, year: int = 2019):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
+    orbit_map = _parse_orbit_map(file_path=input_file_path)
+    from_you = []
+    from_san = []
+    for orbiter, orbited in orbit_map.items():
+        if orbiter == "YOU":
+            current_key = orbited
+            from_you.append(current_key)
+            while current_key in orbit_map.keys():
+                current_key = orbit_map[current_key]
+                from_you.append(current_key)
+        elif orbiter == "SAN":
+            current_key = orbited
+            from_san.append(current_key)
+            while current_key in orbit_map.keys():
+                current_key = orbit_map[current_key]
+                from_san.append(current_key)
+        else:
+            continue
+
+    orbital_transfers = 0
+    for orb in from_you:
+        orbital_transfers += 1
+        if orb in from_san:
+            orbital_transfers += from_san.index(orb) - 1
+            break
+
+    return orbital_transfers
 
 
-# part_two(file="eg")
-# part_two(file="input")
+part_two(file="eg2")
+part_two(file="input")
