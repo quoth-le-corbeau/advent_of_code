978c139430dc451e4efe8650f9bc4094ce1ae515
andrew-x1-ubu <amgorman@tutanota.com>
2025-05-25 16:46:46 +0200

    begin part 2

--- advent_2019/day_7/solutions.py
+++ advent_2019/day_7/solutions.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from reusables import timer, INPUT_PATH
 
 _PHASE_SETTINGS = [0, 1, 2, 3, 4]
+_PHASE_SETTINGS_FEEDBACK_MODE = [5, 6, 7, 8, 9]
 
 
 def _parse_file(file_path: Path) -> list[int]:
@@ -124,13 +125,103 @@ part_one(file="eg")
 part_one(file="input")
 
 
+def _run_in_feedback_mode(
+    program: list[int],
+    phase_input: int,
+    previous_output: int = 0,
+    phase_input_entered: bool = False,
+) -> int:
+    # consider doing a loop counter for input regulation
+    output = -1
+    pointer = 0
+    input_counter = 0
+    while pointer < len(program) - 2:
+        position_zero = str(program[pointer])
+        while len(position_zero) < 5:
+            position_zero = "0" + position_zero
+        opcode = int(position_zero[-2:])
+        mode_1 = position_zero[-3]
+        mode_2 = position_zero[-4]
+        if opcode == 99:
+            print("got opcode 99")
+            break
+        elif opcode == 1:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            program[program[pointer + 3]] = param_1 + param_2
+            pointer += 4
+        elif opcode == 2:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            program[program[pointer + 3]] = param_1 * param_2
+            pointer += 4
+        elif opcode == 3:
+            if input_counter > 2:
+                raise ValueError("There should only be two inputs for each run!")
+            if phase_input_entered:
+                assert input_counter == 1
+                program[program[pointer + 1]] = previous_output
+            else:
+                program[program[pointer + 1]] = phase_input
+                phase_input_entered = True
+                input_counter += 1
+            pointer += 2
+        elif opcode == 4:
+            param_1 = (
+                program[program[pointer + 1]] if mode_1 == "0" else program[pointer + 1]
+            )
+            output = param_1
+            pointer += 2
+        elif opcode == 5:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            if param_1 != 0:
+                pointer = param_2
+            else:
+                pointer += 3
+        elif opcode == 6:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            if param_1 == 0:
+                pointer = param_2
+            else:
+                pointer += 3
+        elif opcode == 7:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            program[program[pointer + 3]] = 1 if param_1 < param_2 else 0
+            pointer += 4
+        elif opcode == 8:
+            param_1, param_2 = _get_param_values(pointer, mode_1, mode_2, program)
+            program[program[pointer + 3]] = 1 if param_1 == param_2 else 0
+            pointer += 4
+        else:
+            raise ValueError(f"Invalid opcode: {opcode}")
+    if output == -1:
+        raise ValueError("No diagnostic output")
+    return output
+
+
+def _get_max_thrust_permutation_feedback_mode(program: list[int]) -> tuple[int, tuple]:
+    phase_settings_by_thrust = defaultdict(tuple)
+    for phase_inputs in permutations(_PHASE_SETTINGS_FEEDBACK_MODE):
+        next_input = 0
+        fresh_program = program.copy()
+        for phase_input in phase_inputs:
+            while True:
+                output = _run_in_feedback_mode(
+                    program=fresh_program,
+                    phase_input=phase_input,
+                    previous_output=next_input,
+                )
+                next_input = output
+        phase_settings_by_thrust[next_input] = tuple(phase_inputs)
+    max_thrust = int(max(phase_settings_by_thrust.keys()))
+    return max_thrust, phase_settings_by_thrust[max_thrust]
+
+
 @timer
 def part_two(file: str, day: int = 7, year: int = 2019):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
     program = _parse_file(file_path=input_file_path)
-    return _run(program=program)
+    max_thrust, phase_setting = _get_max_thrust_permutation_feedback_mode(program)
 
 
 # part_two(file="eg")
--- test/test_2019_day_7.py
+++ test/test_2019_day_7.py
@@ -1,5 +1,8 @@
 from reusables import dict_parametrize
-from advent_2019.day_7.solutions import _get_max_thrust_permutation
+from advent_2019.day_7.solutions import (
+    _get_max_thrust_permutation,
+    _get_max_thrust_permutation_feedback_mode,
+)
 
 TEST_PROG_2 = [
     3,
@@ -64,6 +67,96 @@ TEST_PROG_3 = [
     0,
     0,
 ]
+TEST_FEEDBACK_PROG_1 = [
+    3,
+    26,
+    1001,
+    26,
+    -4,
+    26,
+    3,
+    27,
+    1002,
+    27,
+    2,
+    27,
+    1,
+    27,
+    26,
+    27,
+    4,
+    27,
+    1001,
+    28,
+    -1,
+    28,
+    1005,
+    28,
+    6,
+    99,
+    0,
+    0,
+    5,
+]
+TEST_FEEDBACK_PROG_2 = [
+    3,
+    52,
+    1001,
+    52,
+    -5,
+    52,
+    3,
+    53,
+    1,
+    52,
+    56,
+    54,
+    1007,
+    54,
+    5,
+    55,
+    1005,
+    55,
+    26,
+    1001,
+    54,
+    -5,
+    54,
+    1105,
+    1,
+    12,
+    1,
+    53,
+    54,
+    53,
+    1008,
+    54,
+    0,
+    55,
+    1001,
+    55,
+    1,
+    55,
+    2,
+    53,
+    55,
+    53,
+    4,
+    53,
+    1001,
+    56,
+    -1,
+    56,
+    1005,
+    56,
+    6,
+    99,
+    0,
+    0,
+    0,
+    0,
+    10,
+]
 
 
 @dict_parametrize(
@@ -89,5 +182,29 @@ def test__get_max_thrust_permutation(
     program, expected_max_thrust, expected_max_thrust_phase_setting
 ):
     max_thrust, phase_setting = _get_max_thrust_permutation(program=program)
+    assert max_thrust == expected_max_thrust
+    assert phase_setting == expected_max_thrust_phase_setting
+
+
+@dict_parametrize(
+    {
+        "139629729": {
+            "program": TEST_FEEDBACK_PROG_1,
+            "expected_max_thrust": 139629729,
+            "expected_max_thrust_phase_setting": (9, 8, 7, 6, 5),
+        },
+        "18216": {
+            "program": TEST_FEEDBACK_PROG_2,
+            "expected_max_thrust": 18216,
+            "expected_max_thrust_phase_setting": (9, 7, 8, 5, 6),
+        },
+    }
+)
+def test__get_max_thrust_permutation_feedback_mode(
+    program, expected_max_thrust, expected_max_thrust_phase_setting
+):
+    max_thrust, phase_setting = _get_max_thrust_permutation_feedback_mode(
+        program=program
+    )
     assert max_thrust == expected_max_thrust
     assert phase_setting == expected_max_thrust_phase_setting
