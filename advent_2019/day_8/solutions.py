ba2e720bad69de4699798cb15845dd5b944af649
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-05-27 14:45:15 +0200

    day 8 part 1

--- advent_2019/day_8/solutions.py
+++ advent_2019/day_8/solutions.py
@@ -2,23 +2,39 @@ from pathlib import Path
 
 from reusables import timer, INPUT_PATH
 
+_PIXELS_WIDE = 25
+_PIXELS_HIGH = 6
+_LAYER_SIZE = _PIXELS_WIDE * _PIXELS_HIGH
 
-def RENAME_FUNC(file_path: Path):
+
+def _parse(file_path: Path):
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        return puzzle_input.read().strip()
 
 
 @timer
-def part_one(file: str, day: int = 8, year: int = 2019):
+def part_one(file: str, day: int = 8, year: int = 2019, layer_size: int = _LAYER_SIZE):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    print(f"<-----------{input_file_path} -------------->")
-    print(f"part 1: {RENAME_FUNC(file_path=input_file_path)}")
+
+    layers = _get_layers(
+        pixels=_parse(file_path=input_file_path), layer_size=layer_size
+    )
+    least_zeros_layer = min(layers, key=lambda x: x.count("0"))
+    return least_zeros_layer.count("1") * least_zeros_layer.count("2")
 
 
-part_one(file="eg")
+def _get_layers(pixels: str, layer_size: int) -> list[str]:
+    i = 0
+    layers = []
+    while i <= len(pixels) - layer_size:
+        layers.append(pixels[i : i + layer_size])
+        i += layer_size
+    return layers
+
+
+part_one(file="eg", layer_size=6)
 part_one(file="input")
 
 
@@ -27,8 +43,7 @@ def part_two(file: str, day: int = 8, year: int = 2019):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    print(f"part 2: {RENAME_FUNC(file_path=input_file_path)}")
 
 
-part_two(file="eg")
-part_two(file="input")
+# part_two(file="eg")
+# part_two(file="input")
