ed151a1f45437e674db5ee44476765c289a33fbb
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-06-02 16:31:08 +0200

    get vectors still wrong

--- advent_2019/day_10/solutions.py
+++ advent_2019/day_10/solutions.py
@@ -35,24 +35,57 @@ def _get_vectors(point: tuple[int, int], rows: int, cols: int) -> set[tuple[int,
     return vectors
 
 
-def _filter(all_vectors):
+def _filter(all_vectors: set[tuple[int, int]]) -> set[tuple[int, int]]:
     vectors = set()
     for vector in all_vectors:
-        i, j = vector
+        x, y = vector
         if (
-            (abs(i) == abs(j) and abs(i) != 1)
-            or (j == 0 and abs(i) != 1)
-            or (i == 0 and abs(j) != 1)
+            (abs(x) == abs(y) and abs(x) != 1)
+            or (y == 0 and abs(x) != 1)
+            or (x == 0 and abs(y) != 1)
+            or _is_scalar_multiple(vector=vector, all_vectors=all_vectors)
         ):
             continue
         vectors.add(vector)
     return vectors
 
 
-def _count_visible(
-    vectors: list[tuple[int, int]], look_up: dict[tuple[int, int], bool]
-) -> int:
-    pass
+def _is_scalar_multiple(
+    vector: tuple[int, int], all_vectors: set[tuple[int, int]]
+) -> bool:
+    x, y = vector
+    for n in range(2, 1000):
+        if ((x * n), (y * n)) in all_vectors:
+            return True
+    return False
+
+
+def _get_visible(
+    vectors: list[tuple[int, int]],
+    look_up: dict[tuple[int, int], bool],
+    point: tuple[int, int],
+) -> set[tuple[int, int]]:
+    x, y = point
+    seen = set()
+    visible = set()
+    for vector in vectors:
+        i, j = vector
+        n = 1
+        while True:
+            scalar_x = n * i
+            scalar_y = n * j
+            current = (x + scalar_x, y + scalar_y)
+            if current not in look_up:
+                break
+            for k in range(1, n):
+                if (i // k, j // k) in seen:
+                    n += 1
+                    continue
+            if look_up[current]:
+                seen.add((i, j))
+                visible.add(current)
+            n += 1
+    return visible
 
 
 @timer
@@ -63,26 +96,30 @@ def part_one(file: str, day: int = 10, year: int = 2019) -> int:
     look_up, grid = _parse_grid(file_path=input_file_path)
     rows = len(grid)
     cols = len(grid[0])
-    visible_look_up = {k: 0 for k in look_up if look_up[k]}
+    visible_look_up = {k: set() for k in look_up if look_up[k]}
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] != "#":
                 continue
-            # unit_vectors = _get_vectors(point=(c, r), rows=rows, cols=cols)
+            vectors = _get_vectors(point=(c, r), rows=rows, cols=cols)
+            visible_look_up[(c, r)] |= _get_visible(
+                vectors=sorted(list(vectors)), look_up=look_up, point=(c, r)
+            )
+    print(f"{visible_look_up=}")
+    print(f"{visible_look_up[(3, 4)]=}")
+    max_visible_asteroids = 0
+    for v in visible_look_up.values():
+        if len(v) > max_visible_asteroids:
+            max_visible_asteroids = len(v)
+
+    for k, v in visible_look_up.items():
+        if v == max_visible_asteroids:
+            asteroid = k
+    print(f"max visible asteroids: {max_visible_asteroids} at {k}")
+    return max_visible_asteroids
 
 
-#            visible_look_up[(c, r)] = _count_visible(
-#                vectors=unit_vectors, look_up=look_up
-#            )
-#    max_asteroids = max(visible_look_up.values())
-#    for k, v in visible_look_up.items():
-#        if v == max_asteroids:
-#            asteroid = k
-#    print(f"max visible asteroids: {max_asteroids} at {k}")
-#    return max_asteroids
-
-
-# part_one(file="eg")
+part_one(file="eg")
 # part_one(file="eg_1_2_35")
 # part_one(file="eg_5_8_33")
 # part_one(file="eg_6_3_41")
