1f2132d1a76120c8d9e77575c1009cad2dfbeb5b
quoth-the-macbook <amgorman@tutanota.com>
2025-08-07 22:53:53 +0300

    complete day 8

--- advent_2016/day_8/solutions.py
+++ advent_2016/day_8/solutions.py
@@ -11,19 +11,72 @@ def _parse_instructions(file_path: Path) -> list[str]:
         return puzzle_input.read().strip().splitlines()
 
 
-def _pprint_grid(pixels: list[tuple[int, int]], screen_size: tuple[int, int]) -> None:
-    grid = [["."] * screen_size[0]] * screen_size[1]
-    print(grid)
+def _pprint_grid(
+    pixel_map: dict[tuple[int, int], bool], screen_size: tuple[int, int]
+) -> None:
+    # grid = [["."] * screen_size[0]] * screen_size[1] # note this means each row is a copy!
+    grid = [["."] * screen_size[0] for _ in range(screen_size[1])]
+    for position, pixel in pixel_map.items():
+        i, j = position
+        if pixel:
+            grid[j][i] = "#"
+        pass
+    for row in grid:
+        print(" ".join(row))
+    print("=========")
 
 
 @timer
-def part_one(file: str, screen_size: tuple[int, int], day: int = 8, year: int = 2016):
+def part_one(
+    file: str, screen_size: tuple[int, int], day: int = 8, year: int = 2016
+) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
     instructions = _parse_instructions(file_path=input_file_path)
-    print(f"{instructions=}")
-    _pprint_grid(pixels=[], screen_size=screen_size)
+    pixel_map: dict[tuple[int, int], bool] = {
+        (i, j): False for j in range(screen_size[1]) for i in range(screen_size[0])
+    }
+    for instruction in instructions:
+        if instruction[:4] == "rect":
+            i, j = tuple(map(int, instruction.split(" ")[1].split("x")))
+            for r in range(j):
+                for c in range(i):
+                    pixel_map[c, r] = True
+            _pprint_grid(pixel_map=pixel_map, screen_size=screen_size)
+        elif instruction.split(" ")[1] == "column":
+            col = int(instruction.split(" ")[2].split("=")[1])
+            by_ = int(instruction.split(" ")[-1])
+            positions = [k for k, v in pixel_map.items() if k[0] == col and v]
+            mapped_positions = [
+                (p[0], ((p[1] + by_) % screen_size[1])) for p in positions
+            ]
+            for mapped_position in mapped_positions:
+                pixel_map[mapped_position] = True
+            for position in positions:
+                if position not in mapped_positions:
+                    pixel_map[position] = False
+            _pprint_grid(pixel_map=pixel_map, screen_size=screen_size)
+
+        elif instruction.split(" ")[1] == "row":
+            row = int(instruction.split(" ")[2].split("=")[1])
+            by_ = int(instruction.split(" ")[-1])
+            positions = [k for k, v in pixel_map.items() if k[1] == row and v]
+            mapped_positions = [
+                (((p[0] + by_) % screen_size[0]), p[1]) for p in positions
+            ]
+            for mapped_position in mapped_positions:
+                pixel_map[mapped_position] = True
+            for position in positions:
+                if position not in mapped_positions:
+                    pixel_map[position] = False
+            _pprint_grid(pixel_map=pixel_map, screen_size=screen_size)
+
+        else:
+            raise ValueError(f"Unknown instruction: {instruction}. Abort!")
+
+    pixel_positions = [k for k, v in pixel_map.items() if v]
+    return len(pixel_positions)
 
 
 part_one(file="eg", screen_size=EG_SCREEN_SIZE)
@@ -32,11 +85,5 @@ part_one(file="input", screen_size=SCREEN_SIZE)
 
 @timer
 def part_two(file: str, day: int = 8, year: int = 2016):
-    input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
-        year=year, day=day, file=file
-    )
-    print(f"part 2: {_parse_instructions(file_path=input_file_path)}")
-
-
-part_two(file="eg")
-part_two(file="input")
+    # RURCEOEIL
+    pass
