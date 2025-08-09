36fbec27d6715971928ee1c6453bc9e61dbe048c
quoth-the-macbook <amgorman@tutanota.com>
2025-08-09 14:58:39 +0300

    day 9 part 1

--- advent_2016/day_9/solutions.py
+++ advent_2016/day_9/solutions.py
@@ -3,22 +3,58 @@ from pathlib import Path
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_compressed_file(file_path: Path) -> str:
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        return puzzle_input.read().strip()
+
+
+def _parse(marker: str) -> tuple[int, int]:
+    stripped = marker[1:-1].split("x")
+    return int(stripped[0]), int(stripped[1])
 
 
 @timer
-def part_one(file: str, day: int = 9, year: int = 2016):
+def part_one(file: str, day: int = 9, year: int = 2016) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    print(f"<-----------{input_file_path} -------------->")
-    print(f"part 1: {RENAME_FUNC(file_path=input_file_path)}")
+    compressed = _parse_compressed_file(file_path=input_file_path)
+    print(f"{compressed=}")
+    decompressed = ""
+    pointer = 0
+    while pointer < len(compressed):
+        char = compressed[pointer]
+        if char == "(":
+            marker = ""
+            i = 0
+            while compressed[pointer + i] != ")":
+                marker += compressed[pointer + i]
+                i += 1
+            pointer += i
+            end_marker = compressed[pointer]
+            if end_marker != ")":
+                raise ValueError(f"Unknown end marker: {end_marker}")
+            marker = marker + end_marker
+            number_of_chars_to_repeat, repetitions = _parse(marker)
+            pointer += 1
+            end_pointer = pointer + number_of_chars_to_repeat
+            to_repeat = compressed[pointer:end_pointer]
+            decompressed += to_repeat * repetitions
+            pointer = end_pointer - 1
+        else:
+            decompressed += char
+        pointer += 1
+
+    print(f"{decompressed=}")
+    return len(decompressed)
 
 
 part_one(file="eg")
+part_one(file="eg2")
+part_one(file="eg3")
+part_one(file="eg4")
+part_one(file="eg5")
+part_one(file="eg6")
 part_one(file="input")
 
 
@@ -27,8 +63,7 @@ def part_two(file: str, day: int = 9, year: int = 2016):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    print(f"part 2: {RENAME_FUNC(file_path=input_file_path)}")
 
 
-part_two(file="eg")
-part_two(file="input")
+# part_two(file="eg")
+# part_two(file="input")
