204e4e52382f80bcf7e8e9713e1dfd90a3c9037d
quoth-the-macbook <amgorman@tutanota.com>
2025-08-09 14:58:39 +0300

    prepare day 9

--- advent_2016/day_9/solutions.py
+++ advent_2016/day_9/solutions.py
@@ -19,6 +19,11 @@ def part_one(file: str, day: int = 9, year: int = 2016):
 
 
 part_one(file="eg")
+part_one(file="eg2")
+part_one(file="eg3")
+part_one(file="eg4")
+part_one(file="eg5")
+part_one(file="eg6")
 part_one(file="input")
 
 
@@ -27,8 +32,7 @@ def part_two(file: str, day: int = 9, year: int = 2016):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    print(f"part 2: {RENAME_FUNC(file_path=input_file_path)}")
 
 
-part_two(file="eg")
-part_two(file="input")
+# part_two(file="eg")
+# part_two(file="input")
