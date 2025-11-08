359cc8784cc77c3d9adc9309aca209e9e48437b0
quoth-the-macbook <amgorman@tutanota.com>
2025-11-08 07:39:08 +0100

    day 2 p1

--- advent_2015/day_2/solutions.py
+++ advent_2015/day_2/solutions.py
@@ -1,20 +1,25 @@
 from pathlib import Path
-
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_dimensions(file_path: Path) -> int:
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        lines = puzzle_input.read().strip().splitlines()
+    total = 0
+    for line in lines:
+        l, w, b = line.split("x")
+        sides = [int(l) * int(w), int(w) * int(b), int(l) * int(b)]
+        total += 2 * sum(sides) + min(sides)
+
+    return total
 
 
 @timer
-def part_one(file: str, day: int = 2, year: int = 2015):
+def part_one(file: str, day: int = 2, year: int = 2015) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    return _parse_dimensions(file_path=input_file_path)
 
 
 part_one(file="eg")
@@ -26,7 +31,7 @@ def part_two(file: str, day: int = 2, year: int = 2015):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    return _parse_dimensions(file_path=input_file_path)
 
 
 part_two(file="eg")
