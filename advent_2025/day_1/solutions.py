b3d2a9a84530fe87244ddbd5f04aa949007126b9
quoth-the-macbook <amgorman@tutanota.com>
2025-12-01 07:17:34 +0100

    day 1 2025

--- advent_2025/day_1/solutions.py
+++ advent_2025/day_1/solutions.py
@@ -3,10 +3,9 @@ from pathlib import Path
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_input(file_path: Path) -> list[str]:
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        return puzzle_input.read().strip().splitlines()
 
 
 @timer
@@ -14,7 +13,21 @@ def part_one(file: str, day: int = 1, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    instructions = _parse_input(file_path=input_file_path)
+    print(f"{instructions=}")
+    current = 50
+    count = 0
+    for instruction in instructions:
+        direction = instruction[0]
+        steps = int(instruction[1:])
+        if direction == "L":
+            current -= steps
+        elif direction == "R":
+            current += steps
+        current = current % 100
+        if current == 0:
+            count += 1
+    return count
 
 
 part_one(file="eg")
@@ -26,7 +39,26 @@ def part_two(file: str, day: int = 1, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    instructions = _parse_input(file_path=input_file_path)
+    current = 50
+    count = 0
+    for instruction in instructions:
+        direction = instruction[0]
+        steps = int(instruction[1:])
+        i = 1
+        while i <= steps:
+            if direction == "L":
+                current -= 1
+            elif direction == "R":
+                current += 1
+            if current < 0:
+                current = current % 100
+            if current >= 100:
+                current = current % 100
+            if current == 0:
+                count += 1
+            i += 1
+    return count
 
 
 part_two(file="eg")
