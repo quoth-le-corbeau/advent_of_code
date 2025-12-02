f2fa717ed6238fbe7a9c9482e57052ce52aee957
quoth-the-macbook <amgorman@tutanota.com>
2025-12-02 09:19:17 +0100

    day 2 2025

--- advent_2025/day_2/solutions.py
+++ advent_2025/day_2/solutions.py
@@ -1,20 +1,45 @@
 from pathlib import Path
 
+
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_input(file_path: Path) -> list[str]:
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        ranges = puzzle_input.read().strip().split(",")
+        return ranges
+
+
+def _is_composed_of_repeated_digits(number_str: str) -> bool:
+    num_length = len(number_str)
+    mid = num_length // 2
+    factor_pairs = [
+        (i, num_length // i) for i in range(1, mid + 1) if num_length % i == 0
+    ]
+    for pair in factor_pairs:
+        potential_pattern_length, required_repetitions = pair
+        potential_pattern = number_str[:potential_pattern_length]
+        if number_str == potential_pattern * required_repetitions:
+            return True
+    return False
 
 
 @timer
-def part_one(file: str, day: int = 2, year: int = 2025):
+def part_one(file: str, day: int = 2, year: int = 2025) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    ranges = _parse_input(file_path=input_file_path)
+    print(f"{ranges=}")
+    invalid = []
+    for r in ranges:
+        s, e = int(r.split("-")[0]), int(r.split("-")[1])
+        for n in range(s, e + 1):
+            sn = str(n)
+            mid = len(sn) // 2
+            if sn[:mid] == sn[mid:]:
+                invalid.append(n)
+    return sum(invalid)
 
 
 part_one(file="eg")
@@ -22,11 +47,19 @@ part_one(file="input")
 
 
 @timer
-def part_two(file: str, day: int = 2, year: int = 2025):
+def part_two(file: str, day: int = 2, year: int = 2025) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    ranges = _parse_input(file_path=input_file_path)
+    invalid = []
+    for r in ranges:
+        s, e = int(r.split("-")[0]), int(r.split("-")[1])
+        for n in range(s, e + 1):
+            sn = str(n)
+            if _is_composed_of_repeated_digits(sn):
+                invalid.append(n)
+    return sum(invalid)
 
 
 part_two(file="eg")
