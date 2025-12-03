160b11c8ff8d3fa66332de5bf87cc2e181f4d2f0
quoth-the-macbook <amgorman@tutanota.com>
2025-12-03 07:43:32 +0100

    day 3 part 2 weird recursion saved

--- advent_2025/day_3/weird_recursion_cock_up.py
+++ advent_2025/day_3/weird_recursion_cock_up.py
@@ -11,12 +11,15 @@ def _parse_input(file_path: Path) -> list[list[int]]:
     return banks
 
 
-def _find_largest_two_digit_voltage(bank: list[int]) -> int:
+def _find_largest_twelve_digit_voltage(bank: list[int], num_str: str = "") -> str:
+    if len(num_str) == 12:
+        return num_str
     max_t = len(bank) - 1
     max_u = len(bank)
     tens = 0
     tens_i = 0
     units = 0
+    units_i = 0
     for t in range(max_t):
         if bank[t] > tens:
             tens = bank[t]
@@ -24,28 +27,11 @@ def _find_largest_two_digit_voltage(bank: list[int]) -> int:
     for u in range(max_u):
         if bank[u] > units and u > tens_i:
             units = bank[u]
-    return tens * 10 + units
-
-
-def _find_largest_twelve_digit_voltage(bank: list[int]) -> int:
-    pass
-
-
-@timer
-def part_one(file: str, day: int = 3, year: int = 2025):
-    input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
-        year=year, day=day, file=file
-    )
-    banks = _parse_input(file_path=input_file_path)
-    total = 0
-    for bank in banks:
-        largest = _find_largest_two_digit_voltage(bank)
-        total += int(largest)
-    return total
-
-
-# part_one(file="eg")
-# part_one(file="input")
+            units_i = u
+    n = tens * 10 + units
+    bank = bank[:tens_i] + bank[tens_i + 1 :]
+    bank = bank[: units_i - 1] + bank[units_i:]
+    return _find_largest_twelve_digit_voltage(bank, num_str + str(n))
 
 
 @timer
@@ -57,7 +43,7 @@ def part_two(file: str, day: int = 3, year: int = 2025):
     total = 0
     for bank in banks:
         largest = _find_largest_twelve_digit_voltage(bank)
-        total += largest
+        total += int(largest)
     return total
 
 
