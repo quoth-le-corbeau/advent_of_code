9929977fecd06a1c69058ac1a2c4a75a495e401b
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-12-03 10:10:51 +0100

    unify solutions for both parts

--- advent_2025/day_3/solutions.py
+++ advent_2025/day_3/solutions.py
@@ -11,35 +11,19 @@ def _parse_input(file_path: Path) -> list[list[int]]:
     return banks
 
 
-def _find_largest_two_digit_voltage(bank: list[int]) -> int:
-    max_t = len(bank) - 1
-    max_u = len(bank)
-    tens = 0
-    tens_i = 0
-    units = 0
-    for t in range(max_t):
-        if bank[t] > tens:
-            tens = bank[t]
-            tens_i = t
-    for u in range(max_u):
-        if bank[u] > units and u > tens_i:
-            units = bank[u]
-    return tens * 10 + units
-
-
-def _find_largest_twelve_digit_voltage(bank: list[int], digits_left: int = 11) -> int:
+def _find_largest_n_digit_joltage(bank: list[int], digits: int) -> int:
     total = 0
     digit_count = 0
-    max_i = -1
+    last_index = -1
+    digits_left = digits - 1
     while digits_left >= 0:
         max_ = 0
         pool = bank[digit_count : len(bank) - digits_left]
         for i, n in enumerate(pool):
-            if n > max_ and i >= max_i:
+            if n > max_ and i >= last_index:
                 max_ = n
-                max_i = i
+                last_index = i
         total += max_ * 10**digits_left
-        print(f"subtotal: {total}")
         digit_count += 1
         digits_left -= 1
 
@@ -54,13 +38,13 @@ def part_one(file: str, day: int = 3, year: int = 2025):
     banks = _parse_input(file_path=input_file_path)
     total = 0
     for bank in banks:
-        largest = _find_largest_two_digit_voltage(bank)
+        largest = _find_largest_n_digit_joltage(bank, digits=2)
         total += int(largest)
     return total
 
 
-# part_one(file="eg")
-# part_one(file="input")
+part_one(file="eg")
+part_one(file="input")
 
 
 @timer
@@ -71,10 +55,7 @@ def part_two(file: str, day: int = 3, year: int = 2025):
     banks = _parse_input(file_path=input_file_path)
     total = 0
     for bank in banks:
-        largest = _find_largest_twelve_digit_voltage(bank)
-        print("--------------")
-        print(f"TOTAL: {largest}")
-        print("--------------")
+        largest = _find_largest_n_digit_joltage(bank, digits=12)
         total += largest
     return total
 
