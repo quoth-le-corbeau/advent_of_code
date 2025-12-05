d2654f2cdea25a53aef736ed01a7e65a12f6103f
quoth-the-macbook <amgorman@tutanota.com>
2025-12-05 07:41:49 +0100

    day 5 p1. p2 quick go did not work

--- advent_2025/day_5/solutions.py
+++ advent_2025/day_5/solutions.py
@@ -3,10 +3,15 @@ from pathlib import Path
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_input(file_path: Path):
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        ingredient_ids, ingredients = puzzle_input.read().split("\n\n")
+        all_ingredients = list(map(int, ingredients.strip().splitlines()))
+        id_ranges = [
+            (int(line.split("-")[0]), int(line.split("-")[1]))
+            for line in ingredient_ids.strip().splitlines()
+        ]
+    return sorted(id_ranges, key=lambda x: x[0]), all_ingredients
 
 
 @timer
@@ -14,7 +19,12 @@ def part_one(file: str, day: int = 5, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    id_ranges, ingredients = _parse_input(file_path=input_file_path)
+    count = 0
+    for i in ingredients:
+        if any([a <= i <= b for a, b in id_ranges]):
+            count += 1
+    return count
 
 
 part_one(file="eg")
@@ -26,8 +36,16 @@ def part_two(file: str, day: int = 5, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    id_ranges, _ = _parse_input(file_path=input_file_path)
+    preliminary = id_ranges[-1][1] - id_ranges[0][0] + 1
+    deltas = []
+    for range_pair in zip(id_ranges[:-1], id_ranges[1:]):
+        first, second = range_pair
+        if second[0] > first[1]:
+            deltas.append(second[0] - first[1] - 1)
+    return preliminary - sum(deltas)
 
 
 part_two(file="eg")
 part_two(file="input")
+# 315004568769605
