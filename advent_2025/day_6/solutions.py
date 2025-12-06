c2a079bbce9a1f20d9e263563d47e59f3a7b70f9
quoth-the-macbook <amgorman@tutanota.com>
2025-12-06 09:23:05 +0100

    extremely beautiful solution to part 2 (fuck you for this puzzle btw Eric)

--- advent_2025/day_6/solutions.py
+++ advent_2025/day_6/solutions.py
@@ -4,8 +4,8 @@ from reusables import timer, INPUT_PATH
 
 
 def _parse_input(file_path: Path, n: int) -> list[list[int | str]]:
+    rows = []
     with open(file_path, "r") as puzzle_input:
-        rows = []
         for i, line in enumerate(puzzle_input.read().splitlines()):
             if i == n:
                 symbols = list(re.findall("[+,*]", line))
@@ -53,17 +53,107 @@ def part_one(file: str, day: int = 6, year: int = 2025, n: int = 4):
         year=year, day=day, file=file
     )
     math_problems = _parse_input(file_path=input_file_path, n=n)
-    print(math_problems)
     return sum(_solve(math_problems, n))
 
 
-# part_one(file="eg", n=3)
-# part_one(file="input", n=4)
+part_one(file="eg", n=3)
+part_one(file="input", n=4)
 
 
-def _parse_input_p2(file_path: Path, n: int) -> list[list[int | str]]:
+def _parse_input_p2(file_path: Path) -> list[list[str]]:
     with open(file_path, "r") as puzzle_input:
-        rows = []
+        lines = puzzle_input.read().replace(" ", ".")
+        rows = [line.replace(" ", ".") for line in lines.split("\n")]
+        row_len = _find_row_length(lines)
+
+        # find break points
+        delimiter_indexes = []
+        for i in range(row_len):
+            if all(row[i] == "." for row in rows):
+                delimiter_indexes.append(i)
+        print(f"{delimiter_indexes}")
+
+        parsed = []
+        current_i = 0
+        for i, delimiter_index in enumerate(delimiter_indexes):
+            if i == len(delimiter_indexes):
+                end = row_len
+            else:
+                end = delimiter_index
+            for row in rows:
+                to_append = row[current_i:end]
+                parsed.append(to_append)
+            current_i = delimiter_index
+
+        groups = []
+        current_i = 0
+        for j, p in enumerate(parsed):
+            if "+" in p or "*" in p:
+                if "+" in p and "*" in p:
+                    print(f"WTFK??? j={j} p={p}")
+                group = parsed[current_i : j + 1]
+                groups.append(group)
+                current_i = j + 1
+    return groups
+
+
+def _find_row_length(lines):
+    i = 0
+    while lines[i] != "\n":
+        i += 1
+    return i
+
+
+def _solve_p2(math_problems: list[list[str]]) -> list[int]:
+    s = []
+    for mp in math_problems:
+        s.append(_cephalopod_transform_solve(mp))
+    return s
+
+
+def _cephalopod_transform_solve(problem: list[str]) -> int:
+    powers = len(problem[0])
+    op_line = problem[-1]
+    if "+" in op_line:
+        op = "+"
+    elif "*" in op_line:
+        op = "*"
+    else:
+        raise ValueError("unknown op")
+    try:
+        assert op in ["+", "*"]
+    except AssertionError:
+        print("help")
+    nums = []
+    for i in range(powers):
+        n = ""
+        for prob in problem[:-1]:
+            n += prob[powers - i - 1]
+        nums.append(n)
+    actually_nums = []
+    for n in nums:
+        try:
+            actually_nums.append(int(n.replace(".", "")))
+        except (ValueError, TypeError):
+            print(f"TypeError:{n}")
+    print(f"{actually_nums=}")
+    return _cephalopod_solve(actually_nums, op)
+
+
+def _cephalopod_solve(nums: list[int], op: str) -> int:
+    match op:
+        case "*":
+            r = 1
+            for n in nums:
+                r *= n
+
+        case "+":
+            r = 0
+            for n in nums:
+                r += n
+        case _:
+            raise ValueError(f"unknown op: {op}")
+    return r
 
 
 @timer
@@ -71,8 +161,11 @@ def part_two(file: str, day: int = 6, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return _parse_input_p2(file_path=input_file_path)
+    math_problems = _parse_input_p2(file_path=input_file_path)
+    print(f"{math_problems=}")
+    return sum(_solve_p2(math_problems))
 
 
-part_two(file="eg")
-# part_two(file="input")
+# part_two(file="eg")
+part_two(file="input")
+print(f"solution: {10442199702583 + 477 + 7737}")
