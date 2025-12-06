8efa65571d0c5497dabbe01b03112fbef8e69dfa
quoth-the-macbook <amgorman@tutanota.com>
2025-12-06 09:40:58 +0100

    prepare day 7 remove unnecessary directories

--- advent_2025/day_7/solutions.py
+++ advent_2025/day_7/solutions.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_input(file_path: Path):
     with open(file_path, "r") as puzzle_input:
         lines = puzzle_input.read()
         print(lines)
@@ -14,11 +14,11 @@ def part_one(file: str, day: int = 7, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    return _parse_input(file_path=input_file_path)
 
 
 part_one(file="eg")
-part_one(file="input")
+# part_one(file="input")
 
 
 @timer
@@ -26,8 +26,8 @@ def part_two(file: str, day: int = 7, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    return _parse_input(file_path=input_file_path)
 
 
-part_two(file="eg")
-part_two(file="input")
+# part_two(file="eg")
+# part_two(file="input")
