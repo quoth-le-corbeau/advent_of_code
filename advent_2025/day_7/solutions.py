9b023d1e702a59e7e437ed1f42b944f7b12de3f2
quoth-the-macbook <amgorman@tutanota.com>
2025-12-07 09:59:58 +0100

    solves p2 eg but explodes

--- advent_2025/day_7/solutions.py
+++ advent_2025/day_7/solutions.py
@@ -1,3 +1,4 @@
+from collections import deque, defaultdict
 from pathlib import Path
 
 from reusables import timer, INPUT_PATH
@@ -19,10 +20,10 @@ def _parse_grid(file_path: Path):
                 else:
                     assert char == "."
                     continue
-    print(f"{sorted(list(splitters))=}")
-    print(f"There are {len(splitters)} splitters in total.")
-    print(f"The grid has {i+1} cols and {j+1} rows")
-    print(f"Tachyon beam starts at {s}")
+    # print(f"{sorted(list(splitters))=}")
+    # print(f"There are {len(splitters)} splitters in total.")
+    # print(f"The grid has {i+1} cols and {j+1} rows")
+    # print(f"Tachyon beam starts at {s}")
     return s, splitters, i + 1, j + 1
 
 
@@ -37,6 +38,7 @@ def _dfs(
         marked_splitters = set()
     x, y = current_position
     if y < 0 or y >= rows or x < 0 or x >= cols:
+        assert y == rows
         return marked_splitters
 
     if current_position in splitters:
@@ -94,12 +96,40 @@ def part_one(file: str, day: int = 7, year: int = 2025):
     )
     start, nodes, cols, rows = _parse_grid(file_path=input_file_path)
     hit_splitters = _dfs(current_position=start, cols=cols, rows=rows, splitters=nodes)
-    _pprint(rows, cols, nodes, hit_splitters)
+    # _pprint(rows, cols, nodes, hit_splitters)
     return len(hit_splitters)
 
 
-part_one(file="eg")
-part_one(file="input")
+# part_one(file="eg")
+# part_one(file="input")
+
+
+def _bfs(
+    current_position: GridPoint,
+    cols: int,
+    rows: int,
+    splitters: set[GridPoint],
+) -> int:
+    x, y = current_position
+    q = deque([(x, y, 1)])
+    total_paths = 0
+    cache = defaultdict(int)
+    cache[(x, y)] = 1
+    while len(q) != 0:
+        node = q.popleft()
+
+        x, y, n = node
+        if y >= rows - 1:
+            total_paths += n
+            continue
+
+        if 0 <= x < cols and 0 <= y < rows:
+            if (x, y) in splitters:
+                q.append((x - 1, y, n))
+                q.append((x + 1, y, n))
+            else:
+                q.append((x, y + 1, n))
+    return total_paths
 
 
 @timer
@@ -107,8 +137,11 @@ def part_two(file: str, day: int = 7, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return _parse_grid(file_path=input_file_path)
+    start, nodes, cols, rows = _parse_grid(file_path=input_file_path)
+    unique_paths = _bfs(current_position=start, cols=cols, rows=rows, splitters=nodes)
+    # print(f"{unique_paths=}")
+    return unique_paths
 
 
-# part_two(file="eg")
-# part_two(file="input")
+part_two(file="eg")
+part_two(file="input")
