ea8e744e74d61a168530cc626668bd312ae99b95
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-12-09 17:39:03 +0100

    try visualising perimeter

--- advent_2025/day_9/solutions.py
+++ advent_2025/day_9/solutions.py
@@ -6,7 +6,7 @@ GridPoint = tuple[int, int]
 
 
 def _parse_coordinates(file_path: Path) -> list[GridPoint]:
-    red_tile_x_y = []
+    red_tile_x_y = list()
     with open(file_path, "r") as puzzle_input:
         lines = puzzle_input.read().strip().splitlines()
         for line in lines:
@@ -30,22 +30,15 @@ def part_one(file: str, day: int = 9, year: int = 2025) -> int:
     areas = sorted(
         [
             (_area(a, b), a, b)
-            for i, a in enumerate(red_tile_coordinates)
-            for b in red_tile_coordinates[i + 1 :]
+            for i, a in enumerate(list(red_tile_coordinates))
+            for b in list(red_tile_coordinates)[i + 1 :]
         ]
     )
     return areas[-1][0]
 
 
-# part_one(file="eg")
-# part_one(file="input")
-
-
-def _form_boundary(red_tiles: list[GridPoint]) -> set[GridPoint]:
-    # shoot out rays in all directions
-
-    # cull these to the given red tiles
-    pass
+part_one(file="eg")
+part_one(file="input")
 
 
 @timer
@@ -56,16 +49,14 @@ def part_two(file: str, day: int = 9, year: int = 2025) -> int:
     red_tile_coordinates: list[GridPoint] = _parse_coordinates(
         file_path=input_file_path
     )
-    boundary = _form_boundary(red_tile_coordinates)
-    areas = sorted(
-        [
-            (_area(a, b), a, b)
-            for i, a in enumerate(red_tile_coordinates)
-            for b in red_tile_coordinates[i + 1 :]
-            # filter by being within boundary
-        ]
-    )
+    print(f"{red_tile_coordinates=}")
+    # perimeter_edges = get_perimeter_edges(points=red_tile_coordinates)
+    # print(f"{perimeter_edges=}")
+
+    # for i, a in enumerate(red_tile_coordinates):
+    #    for b in red_tile_coordinates[i + 1 :]:
+    #        area = _area(a, b)
 
 
 part_two(file="eg")
-# part_two(file="input")
+part_two(file="input")
