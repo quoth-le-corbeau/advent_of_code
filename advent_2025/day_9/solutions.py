04f000a1369020836d99318250c2cd57225bcb3b
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-12-10 09:13:42 +0100

    day 9 part 2 attempt

--- advent_2025/day_9/solutions.py
+++ advent_2025/day_9/solutions.py
@@ -5,14 +5,17 @@ from reusables import timer, INPUT_PATH
 GridPoint = tuple[int, int]
 
 
-def _parse_coordinates(file_path: Path) -> list[GridPoint]:
+def _parse_coordinates(
+    file_path: Path,
+) -> tuple[list[GridPoint], list[tuple[GridPoint, GridPoint]]]:
     red_tile_x_y = list()
+    perimeter_lines = list()
     with open(file_path, "r") as puzzle_input:
         lines = puzzle_input.read().strip().splitlines()
         for line in lines:
             x, y = line.split(",")
             red_tile_x_y.append((int(x), int(y)))
-    return red_tile_x_y
+    return red_tile_x_y, list(zip(red_tile_x_y[:-1], red_tile_x_y[1:]))
 
 
 def _area(a: GridPoint, b: GridPoint) -> int:
@@ -26,7 +29,7 @@ def part_one(file: str, day: int = 9, year: int = 2025) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    red_tile_coordinates = _parse_coordinates(file_path=input_file_path)
+    red_tile_coordinates, _ = _parse_coordinates(file_path=input_file_path)
     areas = sorted(
         [
             (_area(a, b), a, b)
@@ -41,15 +44,28 @@ part_one(file="eg")
 part_one(file="input")
 
 
+def _rectangle_inside_perimter():
+    pass
+
+
 @timer
 def part_two(file: str, day: int = 9, year: int = 2025) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    red_tile_coordinates: list[GridPoint] = _parse_coordinates(
+    red_tile_coordinates, perimeter_lines = _parse_coordinates(
         file_path=input_file_path
     )
     print(f"{red_tile_coordinates=}")
+    print(f"{perimeter_lines=}")
+    areas = sorted(
+        [
+            (_area(a, b), a, b)
+            for i, a in enumerate(list(red_tile_coordinates))
+            for b in list(red_tile_coordinates)[i + 1 :]
+            if _rectangle_inside_perimter(a, b, perimeter_lines)
+        ]
+    )
     # perimeter_edges = get_perimeter_edges(points=red_tile_coordinates)
     # print(f"{perimeter_edges=}")
 
