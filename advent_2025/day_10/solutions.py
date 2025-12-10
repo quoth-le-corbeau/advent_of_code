d695f4782659aeaab3a59b7e67e8b81ffdd2ecfa
quoth-the-macbook <amgorman@tutanota.com>
2025-12-10 08:40:39 +0100

    begin day 10 parsing

--- advent_2025/day_10/solutions.py
+++ advent_2025/day_10/solutions.py
@@ -2,11 +2,50 @@ from pathlib import Path
 
 from reusables import timer, INPUT_PATH
 
+"""
+[....] (3)
+[...#] (1,3)
+[.#..] (2)
+[.##.] done
+"""
 
-def _parse_input(file_path: Path):
+
+def _parse_input(file_path: Path) -> list[list]:
+    configs = []
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        for config_str in [
+            line.split(" ") for line in puzzle_input.read().strip().splitlines()
+        ]:
+            config = []
+            config.append(_parse_lights(config_str[0]))
+            config.append(_parse_buttons(config_str[1:-1]))
+            config.append(_parse_joltages(config_str[-1]))
+            configs.append(config)
+    return configs
+
+
+def _parse_joltages(joltage_str):
+    return tuple(
+        int(j) for j in joltage_str.replace("{", "").replace("}", "").split(",")
+    )
+
+
+def _parse_buttons(button_str):
+    buttons = []
+    for b in button_str:
+        buttons.append(tuple(map(int, b.replace(")", "").replace("(", "").split(","))))
+    return buttons
+
+
+def _parse_lights(light_str):
+    lights = []
+    for l in light_str[1:-1]:
+        if l == ".":
+            lights.append(False)
+        else:
+            assert l == "#"
+            lights.append(True)
+    return lights
 
 
 @timer
@@ -14,11 +53,12 @@ def part_one(file: str, day: int = 10, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return _parse_input(file_path=input_file_path)
+    configs = _parse_input(file_path=input_file_path)
+    print(f"{configs=}")
 
 
 part_one(file="eg")
-part_one(file="input")
+# part_one(file="input")
 
 
 @timer
