470477ac60bf403da546edf2e278074ab049dc86
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-12-10 11:52:48 +0100

    day 9 part 2 works but fails on example wtfk

--- advent_2025/day_9/solutions.py
+++ advent_2025/day_9/solutions.py
@@ -7,21 +7,21 @@ GridPoint = tuple[int, int]
 
 def _parse_coordinates(
     file_path: Path,
-) -> tuple[list[GridPoint], list[tuple[GridPoint, GridPoint]]]:
-    red_tile_x_y = list()
-    perimeter_lines = list()
+) -> list[GridPoint]:
+    red_tile_x_y_coordinates = list()
     with open(file_path, "r") as puzzle_input:
         lines = puzzle_input.read().strip().splitlines()
         for line in lines:
             x, y = line.split(",")
-            red_tile_x_y.append((int(x), int(y)))
-    return red_tile_x_y, list(zip(red_tile_x_y[:-1], red_tile_x_y[1:]))
+            red_tile_x_y_coordinates.append((int(x), int(y)))
+    return red_tile_x_y_coordinates
 
 
 def _area(a: GridPoint, b: GridPoint) -> int:
     x, y = a
     x1, y1 = b
-    return (abs(y - y1) + 1) * (abs(x - x1) + 1)
+    area = (abs(y - y1) + 1) * (abs(x - x1) + 1)
+    return area
 
 
 @timer
@@ -29,7 +29,7 @@ def part_one(file: str, day: int = 9, year: int = 2025) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    red_tile_coordinates, _ = _parse_coordinates(file_path=input_file_path)
+    red_tile_coordinates = _parse_coordinates(file_path=input_file_path)
     areas = sorted(
         [
             (_area(a, b), a, b)
@@ -40,12 +40,71 @@ def part_one(file: str, day: int = 9, year: int = 2025) -> int:
     return areas[-1][0]
 
 
-part_one(file="eg")
-part_one(file="input")
+# part_one(file="eg")
+# part_one(file="input")
 
 
-def _rectangle_inside_perimter():
-    pass
+def _inside_condition(lower: int, upper: int, point: int) -> bool:
+    return lower < point < upper
+
+
+def _rectangle_inside_perimeter(
+    corner: GridPoint,
+    opposite_corner: GridPoint,
+    perimeter_lines: list[tuple[GridPoint, GridPoint]],
+) -> bool:
+    valid = True
+    rectangle_min_x = min(corner[0], opposite_corner[0])
+    rectangle_min_y = min(corner[1], opposite_corner[1])
+    rectangle_max_x = max(corner[0], opposite_corner[0])
+    rectangle_max_y = max(corner[1], opposite_corner[1])
+
+    for perimeter_line in perimeter_lines:
+        (x1, y1), (x2, y2) = perimeter_line
+        # all horizontal or vertical lines so x1 == y1 or y1 == y2
+        p_min_x = min(x1, x2)
+        p_max_x = max(x1, x2)
+        p_min_y = min(y1, y2)
+        p_max_y = max(y1, y2)
+        # guarantee direction of perimeter line N -> E and S -> N (x2 > x1) and (y2 > y1)
+        x1, x2 = p_min_x, p_max_x
+        y1, y2 = p_min_y, p_max_y
+
+        if (
+            _inside_condition(lower=rectangle_min_x, upper=rectangle_max_x, point=x1)
+            and _inside_condition(
+                lower=rectangle_min_y, upper=rectangle_max_y, point=y1
+            )
+            and _inside_condition(
+                lower=rectangle_min_x, upper=rectangle_max_x, point=x2
+            )
+            and _inside_condition(
+                lower=rectangle_min_y, upper=rectangle_max_y, point=y2
+            )
+        ):
+            valid = False
+
+        if (
+            x1 == x2
+            and _inside_condition(
+                lower=rectangle_min_x, upper=rectangle_max_x, point=x1
+            )
+            and y2 > rectangle_min_y
+            and y1 < rectangle_max_y
+        ):
+            valid = False
+
+        if (
+            y1 == y2
+            and _inside_condition(
+                lower=rectangle_min_y, upper=rectangle_max_y, point=y1
+            )
+            and x2 > rectangle_min_x
+            and x1 < rectangle_max_x
+        ):
+            valid = False
+
+    return valid
 
 
 @timer
@@ -53,26 +112,17 @@ def part_two(file: str, day: int = 9, year: int = 2025) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    red_tile_coordinates, perimeter_lines = _parse_coordinates(
-        file_path=input_file_path
-    )
-    print(f"{red_tile_coordinates=}")
-    print(f"{perimeter_lines=}")
-    areas = sorted(
-        [
-            (_area(a, b), a, b)
-            for i, a in enumerate(list(red_tile_coordinates))
-            for b in list(red_tile_coordinates)[i + 1 :]
-            if _rectangle_inside_perimter(a, b, perimeter_lines)
-        ]
-    )
-    # perimeter_edges = get_perimeter_edges(points=red_tile_coordinates)
-    # print(f"{perimeter_edges=}")
+    red_tile_coordinates = _parse_coordinates(file_path=input_file_path)
+    perimeter_lines = list(zip(red_tile_coordinates[:-1], red_tile_coordinates[1:]))
+    valid_areas = [
+        (_area(a, b), a, b)
+        for i, a in enumerate(list(red_tile_coordinates))
+        for b in list(red_tile_coordinates)[i + 1 :]
+        if _rectangle_inside_perimeter(a, b, perimeter_lines)
+    ]
 
-    # for i, a in enumerate(red_tile_coordinates):
-    #    for b in red_tile_coordinates[i + 1 :]:
-    #        area = _area(a, b)
+    return max(valid_areas)[0]
 
 
 part_two(file="eg")
-part_two(file="input")
+part_two(file="input")  # 1554370486
