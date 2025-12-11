7b799748881ee519784f9f029a6e7930d20cf3ed
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-12-11 11:48:24 +0100

    day 11p1

--- advent_2025/day_11/solutions.py
+++ advent_2025/day_11/solutions.py
@@ -1,12 +1,33 @@
+from collections import deque
 from pathlib import Path
 
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_tree(file_path: Path) -> dict[str, set[str]]:
+    tree = dict()
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        for line in puzzle_input.read().strip().splitlines():
+            key, values = line.split(": ")
+            tree[key.strip()] = set(values.split(" "))
+    return tree
+
+
+def _bfs(start_node: str, tree: dict[str, set[str]]):
+    q = deque([[start_node]])
+    # visited = set()
+    paths = []
+    while q:
+        path = q.popleft()
+        current_node = path[-1]
+        if current_node == "out":
+            paths.append(path)
+            continue
+        for neighbor in tree[current_node]:
+            # if neighbor not in visited:
+            q.append(path + [neighbor])
+            # visited.add(neighbor)
+    return paths
 
 
 @timer
@@ -14,7 +35,11 @@ def part_one(file: str, day: int = 11, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    tree = _parse_tree(file_path=input_file_path)
+    print(f"{tree=}")
+    paths = _bfs(start_node="you", tree=tree)
+    print(f"{paths=}")
+    return len(paths)
 
 
 part_one(file="eg")
@@ -26,8 +51,8 @@ def part_two(file: str, day: int = 11, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    return _parse_tree(file_path=input_file_path)
 
 
-part_two(file="eg")
-part_two(file="input")
+# part_two(file="eg")
+# part_two(file="input")
