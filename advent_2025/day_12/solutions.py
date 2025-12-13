5ae5adb57c1fa36b2c23f7f9c03f8740d0d928b3
quoth-the-macbook <amgorman@tutanota.com>
2025-12-13 16:02:34 +0100

    day 12 cheeky try paid off

--- advent_2025/day_12/solutions.py
+++ advent_2025/day_12/solutions.py
@@ -3,10 +3,27 @@ from pathlib import Path
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+def _parse_input(file_path: Path):
+    shapes_by_index = {}
+    area_rule_pairs = []
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        blocks = puzzle_input.read().split("\n\n")
+        for i, block in enumerate(blocks):
+            if i == len(blocks) - 1:
+                for line in block.strip().splitlines():
+                    dimensions, rules = line.strip().split(":")
+                    w, h = dimensions.split("x")
+                    area_rule_pairs.append(
+                        (int(w) * int(h), list(map(int, rules.strip().split())))
+                    )
+            else:
+                idx, shape = block.split(":")
+                shape = shape.strip().splitlines()
+                shape_volume = sum(s.count("#") for s in shape)
+                shapes_by_index[int(idx)] = (shape, shape_volume)
+    print(f"{area_rule_pairs=}")
+    print(f"{shapes_by_index=}")
+    return shapes_by_index, area_rule_pairs
 
 
 @timer
@@ -14,7 +31,15 @@ def part_one(file: str, day: int = 12, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    shapes_by_index, area_rule_pairs = _parse_input(file_path=input_file_path)
+    count = 0
+    for area, rule in area_rule_pairs:
+        rule_volume = 0
+        for idx, n in enumerate(rule):
+            rule_volume += shapes_by_index[idx][1] * n
+        if rule_volume <= area:
+            count += 1
+    return count
 
 
 part_one(file="eg")
@@ -26,8 +51,8 @@ def part_two(file: str, day: int = 12, year: int = 2025):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    return _parse_input(file_path=input_file_path)
 
 
-part_two(file="eg")
-part_two(file="input")
+# part_two(file="eg")
+# part_two(file="input")
