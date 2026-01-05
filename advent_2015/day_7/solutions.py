842c7b2dbc133563fb4303da2df88f3cfbac124c
quoth-the-macbook <amgorman@tutanota.com>
2026-01-05 21:43:14 +0100

    wip

--- advent_2015/day_7/solutions.py
+++ advent_2015/day_7/solutions.py
@@ -3,22 +3,52 @@ from pathlib import Path
 from reusables import timer, INPUT_PATH
 
 
-def RENAME_FUNC(file_path: Path):
+# https://wiki.python.org/moin/BitwiseOperators
+
+
+# Algorithm:
+# represent as graph with a dict:
+eg_dict: dict[str, int | str] = {
+    "d": "x & y",
+    "e": "d + x",
+    "x": 123,
+    "y": 456,
+}
+
+
+def _get_signals(eg_dict: dict[str, int | str]) -> dict[str, int]:
+    res = dict()
+    for k, v in eg_dict.items():
+        if isinstance(v, int):
+            res[k] = v
+        else:
+            assert isinstance(v, str)
+            x, cmd, y = v.split(" ")
+
+
+_get_signals(eg_dict)
+
+
+def _parse_instructions(file_path: Path) -> [str, int | str]:
     with open(file_path, "r") as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        for line in puzzle_input.read().strip().splitlines():
+            cmd, k = line.split(" -> ")
+            # use s.isdigit()
 
 
 @timer
-def part_one(file: str, day: int = 7, year: int = 2015):
+def part_one(file: str, day: int = 7, year: int = 2015) -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    signals_by_wire = dict()
+    for k, v in _parse_instructions(file_path=input_file_path).items():
+        signals_by_wire[k] = eval(v)
+    return signals_by_wire["a"]
 
 
-part_one(file="eg")
-part_one(file="input")
+# part_one(file="eg")
+# part_one(file="input")
 
 
 @timer
@@ -26,8 +56,8 @@ def part_two(file: str, day: int = 7, year: int = 2015):
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    return RENAME_FUNC(file_path=input_file_path)
+    return _parse_instructions(file_path=input_file_path)
 
 
-part_two(file="eg")
-part_two(file="input")
+# part_two(file="eg")
+# part_two(file="input")
