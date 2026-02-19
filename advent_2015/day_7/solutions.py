4ef9596fd24205ac60c38dcb0d7d526ecfee9ba2
andrew gorman <andrew.gorman@hydrogrid.ai>
2026-02-19 11:26:58 +0100

    restart day 7

--- advent_2015/day_7/solutions.py
+++ advent_2015/day_7/solutions.py
@@ -6,48 +6,38 @@ from reusables import timer, INPUT_PATH
 # https://wiki.python.org/moin/BitwiseOperators
 
 
-# Algorithm:
-# represent as graph with a dict:
-eg_dict: dict[str, int | str] = {
-    "d": "x & y",
-    "e": "d + x",
-    "x": 123,
-    "y": 456,
-}
-
-
-def _get_signals(eg_dict: dict[str, int | str]) -> dict[str, int]:
-    res = dict()
-    for k, v in eg_dict.items():
-        if isinstance(v, int):
-            res[k] = v
-        else:
-            assert isinstance(v, str)
-            x, cmd, y = v.split(" ")
-
-
-_get_signals(eg_dict)
-
-
-def _parse_instructions(file_path: Path) -> [str, int | str]:
+def _parse_instructions(file_path: Path):
+    graph = {}
     with open(file_path, "r") as puzzle_input:
         for line in puzzle_input.read().strip().splitlines():
-            cmd, k = line.split(" -> ")
-            # use s.isdigit()
+            op, target = line.split(" -> ")
+            graph[target] = op
+    return graph
+
+
+def _dfs(graph, start: str):
+    print(f"{start=}")
+    command = graph[start]
+    input_1, input_2 = command.split(" ")[0], command.split(" ")[-1]
+    for input in [input_1, input_2]:
+        if graph[input].isdigit():
+            print(f"Bottom! {input} => {graph[input]}")
+        else:
+            _dfs(graph, input)
 
 
 @timer
-def part_one(file: str, day: int = 7, year: int = 2015) -> int:
+def part_one(file: str, day: int = 7, year: int = 2015, wire: str = "a") -> int:
     input_file_path: Path = Path(__file__).resolve().parents[2] / INPUT_PATH.format(
         year=year, day=day, file=file
     )
-    signals_by_wire = dict()
-    for k, v in _parse_instructions(file_path=input_file_path).items():
-        signals_by_wire[k] = eval(v)
-    return signals_by_wire["a"]
+    graph = _parse_instructions(input_file_path)
+    print(f"{graph=}")
+    _dfs(graph, start=wire)
+    return -1
 
 
-# part_one(file="eg")
+part_one(file="eg", wire="e")
 # part_one(file="input")
 
 
