d12e8745d9529526c8fa3626c622645682b33fd3
andrew gorman <andrew.gorman@hydrogrid.ai>
2026-04-17 16:29:10 +0200

    WIP day 7 processing graph

--- advent_2015/day_7/solutions.py
+++ advent_2015/day_7/solutions.py
@@ -11,19 +11,15 @@ def _parse_instructions(file_path: Path):
     with open(file_path, "r") as puzzle_input:
         for line in puzzle_input.read().strip().splitlines():
             op, target = line.split(" -> ")
+            op = (
+                op.replace("LSHIFT", "<<")
+                .replace("RSHIFT", ">>")
+                .replace("AND", "&")
+                .replace("OR", "|")
+                .replace("NOT", "~")
+            )
             graph[target] = op
     return graph
-
-
-def _dfs(graph, start: str):
-    print(f"{start=}")
-    command = graph[start]
-    input_1, input_2 = command.split(" ")[0], command.split(" ")[-1]
-    for input in [input_1, input_2]:
-        if graph[input].isdigit():
-            print(f"Bottom! {input} => {graph[input]}")
-        else:
-            _dfs(graph, input)
 
 
 @timer
@@ -32,13 +28,56 @@ def part_one(file: str, day: int = 7, year: int = 2015, wire: str = "a") -> int:
         year=year, day=day, file=file
     )
     graph = _parse_instructions(input_file_path)
-    print(f"{graph=}")
-    _dfs(graph, start=wire)
+    processed_graph = _replace_with_int_where_possible(graph)
+
+    print(f"Post processing: {processed_graph=}")
+
+    while True:
+        for letter, value in processed_graph.items():
+            if isinstance(value, int):
+                if letter == wire:
+                    print(f"BEFORE RETURN: {processed_graph=}")
+                    return value
+                processed_graph = _replace_with_int_where_possible(processed_graph)
+                continue
+            split_val = value.split(" ")
+            if len(split_val) == 1:
+                continue
+            try:
+                int(split_val[0])
+                int(split_val[-1])
+            except ValueError:
+                continue
+            if len(split_val[0]) > 1 and split_val[0][0] == "0":
+                split_val[0] = split_val[0][1:]
+                value = " ".join(split_val)
+            if len(split_val[-1]) > 1 and split_val[-1][0] == "0":
+                split_val[-1] = split_val[-1][1:]
+                value = " ".join(split_val)
+            processed_graph[letter] = eval(value)
+            processed_graph = _replace_with_int_where_possible(processed_graph)
+            if letter == wire:
+                print(f"BEFORE RETURN: {processed_graph=}")
+                return graph[letter]
+        continue
+
     return -1
 
 
-part_one(file="eg", wire="e")
-# part_one(file="input")
+def _replace_with_int_where_possible(graph: dict[str, str]) -> dict[str, str]:
+    for letter, value in graph.items():
+        try:
+            val = int(value)
+        except ValueError:
+            continue
+        for l, v in graph.items():
+            if not isinstance(v, int) and letter in v:
+                graph[l] = v.replace(letter, str(val))
+    return graph
+
+
+# part_one(file="eg", wire="e")
+part_one(file="input", wire="a")
 
 
 @timer
